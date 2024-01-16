aaa750c2310babe1c4881303105b13f7598dd89c
Anudnya93 <anudnya.durgule@crowdbotics.com>
2024-01-16 13:15:58 +0530

    facebook social signin integration

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,9 +1,14 @@
 import { Alert } from "react-native"
 
 export const handleError = (res) => {
-    console.log({res})
-    const key = Object.keys(res.message)[0]
-    const message = Array.isArray(res.message[key]) ? res.message[key][0] : res.message[key]
+    let message = ''
+    console.log({ res, message: res?.message?.message })
+    if (res?.message?.message) {
+        message = res?.message?.message
+    } else {
+        const key = Object.keys(res.message)[0]
+        message = res?.message?.message || Array.isArray(res.message[key]) ? res.message[key][0] : res.message[key]
+    }
     console.log({ message })
     Alert.alert('Error', message)
 }
