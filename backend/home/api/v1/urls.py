4836f365a7f5ae3251dd4b1571974ad36ece6a65
amitcbt <amit.jha@crowdbotics.com>
2024-01-18 19:04:33 +0530

    token on signup

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -12,7 +12,6 @@ from home.api.v1.viewsets import (
 
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
-# router.register("signup", SignupAndLoginViewSet, basename="signup_login")
 router.register("login", LoginViewSet, basename="login")
 router.register("send-email-otp", SendEmailOtpViewset, basename="send_email_otp")
 router.register("confirm-email-otp", ConfirmEmailOtpViewset, basename="confirm_email_otp")
