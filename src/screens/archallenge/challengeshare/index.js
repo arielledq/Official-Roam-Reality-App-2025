e0d8cdca80be40e09743fbb56c6a06a9cf60f6e2
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-01-21 22:31:24 +0500

    - Share Challenge UI

--- src/screens/archallenge/challengeshare/index.js
+++ src/screens/archallenge/challengeshare/index.js
@@ -10,6 +10,8 @@ import { useNavigation, useRoute } from "@react-navigation/native"
 import AppHeader from "../../../components/header"
 import AppText from "../../../components/text"
 import useStyles from "./styles"
+import ShareImg from "../../../assets/ar/share_img.png"
+import Coke from "../../../assets/ar/sponsored/coke.png"
 
 const ArChallengeShare = ({
 
@@ -23,6 +25,25 @@ const ArChallengeShare = ({
       <AppHeader title={challengeObj?.sponsored?.name} backgroundColor="transparent" />
       <AppText style={[styles.headerText]}>Congrats on completing the {challengeObj?.sponsored?.name} Photo AR Experience! </AppText>
       <AppText style={[styles.subHeaderText]}>Please note you must share your experience to at least one social platform to earn all your points.</AppText>
+      <View style={{ borderRadius: 12, height: 422, width: '100%', backgroundColor: "#272741", marginVertical: 20, overflow: 'hidden' }}>
+        <Image source={ShareImg} style={{ width: '100%', height: 318 }} />
+        <View style={{ width: '100%', height: 104, borderRadius: 20, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 20 }}>
+          <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: "#B816E0", width: 73, height: 63, borderRadius: 8 }}>
+            <Text style={styles.pointCount}>50</Text>
+            <Text style={styles.pointCountText}>Points</Text>
+          </View>
+          <View style={{ paddingHorizontal: 10 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image style={{ width: 24, height: 24, marginEnd: 10 }} source={Coke} />
+              <Text style={styles.challengeSponsorName}>Coca Cola</Text>
+            </View>
+            <View >
+              <Text style={styles.challengeSponsorTipText}>Share your recorded experience for extra credits!</Text>
+              <Text style={styles.challengeSponsorStartDateText}>Started on : 12-12-23</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </BackgroundWithImage>
   )
 }
--- src/screens/archallenge/challengeshare/styles.js
+++ src/screens/archallenge/challengeshare/styles.js
@@ -27,6 +27,35 @@ const useStyles = makeStyles(theme => ({
     color: '#9CA3AF',
     marginTop: 10,
   },
+  pointCount: {
+    ...fontGroup.p900,
+    fontSize: FontSizes.S24,
+    color: theme.colors.white,
+    margin: 0
+  },
+
+  pointCountText: {
+    ...fontGroup.p400,
+    fontSize: FontSizes.S10,
+    color: theme.colors.white,
+  },
+  challengeSponsorName: {
+    ...fontGroup.p700,
+    fontSize: FontSizes.S22,
+    color: theme.colors.white,
+  },
+
+  challengeSponsorTipText: {
+    ...fontGroup.p700,
+    fontSize: FontSizes.S12,
+    color: "#F2F2F2",
+    width:'80%'
+  },
+  challengeSponsorStartDateText: {
+    ...fontGroup.p300,
+    fontSize: FontSizes.S10,
+    color: "#9CA3AF",
+  },
 }))
 
 export default useStyles
