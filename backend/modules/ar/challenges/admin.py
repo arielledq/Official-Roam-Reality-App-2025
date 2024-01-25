025ce3c35923d799a932c7e933a5f7578ec733ec
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-01-25 23:35:42 +0500

    - AR User Profile Challenge

--- backend/modules/ar/challenges/admin.py
+++ backend/modules/ar/challenges/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Challenges, Sponsor, Resource3dModel
+from .models import Challenges, Sponsor, ARUserProfile
 
 class ARChallengeAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Sponsor, ARChallengeAdmin)
 admin.site.register(Challenges, ARChallengeAdmin)
-admin.site.register(Resource3dModel, ARChallengeAdmin)
+admin.site.register(ARUserProfile, ARChallengeAdmin)
