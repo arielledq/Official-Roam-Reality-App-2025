29466bab8981e279a1967101f4a3ff90e23ae496
Nitish-Kes1 <nitish.kesarwani@crowdbotics.com>
2024-01-25 11:54:28 +0530

    navigation setup and added drawer navigation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 /**
  * @format
  */
@@ -5,7 +6,6 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
-import 'react-native-gesture-handler'
 
 AppRegistry.registerComponent(appName, () => App)
 
