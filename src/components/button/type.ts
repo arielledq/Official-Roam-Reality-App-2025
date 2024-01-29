0804fac6f640ae8d9ec841ead65444229f3ef90c
jasandeepCB <jasandeep.singh@crowdbotics.com>
2024-01-29 20:59:12 +0530

    minor changes

--- src/components/button/type.ts
+++ src/components/button/type.ts
@@ -1,3 +1,3 @@
 import { ButtonProps as ButtonPropsRN } from '@rneui/themed';
 
-export type ButtonProps = ButtonPropsRN & {};
+export type ButtonProps = ButtonPropsRN & { customColors?: string[]; };
