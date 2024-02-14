9d597daa5262e733d61e5ad85712be9a0c9e92ae
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-02-14 22:58:35 +0500

    - loaded ar memories

--- src/components/memoryContainer/index.tsx
+++ src/components/memoryContainer/index.tsx
@@ -25,7 +25,7 @@ const MemoryContainer = ({
       <View style={styles.cardInner}>
         <FastImage style={styles.iconStyle}
           resizeMode={FastImage.resizeMode.cover}
-          source={{ uri: item.memory_type == 'VIDEO' ? item?.thumbnail_memory_video_file : item?.image }} />
+          source={{ uri: item.memory_type == 'VIDEO' ? item?.thumbnail_memory_video_file : item?.memory_file }} />
         <View style={styles.cardBottomContent}>
           <AppText style={styles.titleStyle}>{item?.challenge_details?.name}</AppText>
           <AppText numberOfLines={2} style={styles.Text}>{item?.challenge_details?.description.replace(/<[^>]+>/g, '')}</AppText>
