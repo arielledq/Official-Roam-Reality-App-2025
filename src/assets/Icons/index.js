d42bfd8c787092c0935509cae402d02fd6b0ce27
Nitish-Kes1 <nitish.kesarwani@crowdbotics.com>
2024-02-19 00:16:44 +0530

    Add new social account icons and components

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -28,6 +28,9 @@ import SelectedFlagIcon from './SelectedFlagIcon.svg'
 import UnselectedProfile from './UnselectedProfile.svg'
 import SelectedProfile from './SelectedProfile.svg'
 import SelectedCamera from './SelectedCamera.svg'
+import FacebookIcon  from './facebookIcon.svg'
+import Instagram  from './Instagram.svg'
+import TikTok from './TikTok.svg'
 
 export const Icons = {
   password,
@@ -59,5 +62,8 @@ export const Icons = {
   SelectedFlagIcon,
   UnselectedProfile,
   SelectedProfile,
-  SelectedCamera
+  SelectedCamera,
+  FacebookIcon,
+  Instagram,
+  TikTok
 }
