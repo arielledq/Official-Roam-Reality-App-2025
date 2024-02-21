2e3aba5f6a3d308400bb227476a4e2be1704f730
amitcbt <amit.jha@crowdbotics.com>
2024-02-21 10:09:02 +0530

    removed unused model field

--- backend/feedback/api/v1/viewsets.py
+++ backend/feedback/api/v1/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from feedback.models import ContactUs
 
 from feedback.serializers import ContactUsSerializer
@@ -7,6 +9,8 @@ class ContactUsViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing the contact us form.
     """
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = ContactUs.objects.all()
     serializer_class = ContactUsSerializer
     http_method_names = ["post"]
