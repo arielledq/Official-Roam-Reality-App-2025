e3e1dccd848280ff0d7c8bd37193fed07a527ce7
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-02-22 18:40:35 +0500

    - Export Done For Android

--- src/components/memoryContainer/index.tsx
+++ src/components/memoryContainer/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
-import { Image, Pressable, TouchableOpacity, View } from "react-native"
+import { Image, Platform, Pressable, TouchableOpacity, View } from "react-native"
 import useStyles from "./styles"
 import AppText from "../text"
-import Images from "../../assets/images"
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 import FastImage from "react-native-fast-image"
+import DownloadImg from "../../assets/ar/download.svg"
+import RNFetchBlob from "rn-fetch-blob";
 
 const MemoryContainer = ({
   title,
@@ -18,8 +20,46 @@ const MemoryContainer = ({
   onPressAction?: () => void,
   item: any
 }) => {
+
+  const getPathFromUrl = (url: String) => {
+    return url.split("?")[0];
+  }
+
   const styles = useStyles()
-  console.log("item:", item)
+  let memoryURL = item?.memory_file
+  let memoryPath = getPathFromUrl(item?.memory_file)
+  const fileExt = memoryPath.split('.').pop();
+
+  let newMemoryUri = memoryPath.lastIndexOf('/');
+  let memoryName = memoryPath.substring(newMemoryUri);
+
+  let dirs = RNFetchBlob.fs.dirs;
+  let path = Platform.OS === 'ios' ? dirs['MainBundleDir'] + memoryName : dirs.PictureDir + memoryName;
+
+  const saveToGallery = () => {
+    if (Platform.OS == 'android') {
+      console.log("path:", path)
+      console.log("fileExt:", fileExt)
+      RNFetchBlob.config({
+        fileCache: true,
+        appendExt: fileExt,
+        indicator: true,
+        IOSBackgroundTask: true,
+        path: path,
+        addAndroidDownloads: {
+          useDownloadManager: true,
+          notification: true,
+          path: path,
+          description: fileExt == 'mp4' ? 'Video' : "Image"
+        },
+      }).fetch("GET", memoryURL).then(res => {
+        console.log(res, 'end downloaded')
+      });
+    } else {
+      CameraRoll.saveToCameraRoll(memoryURL);
+    }
+  }
+
   return (
     <Pressable style={styles.cardContainer} onPress={onPressAction}>
       <View style={styles.cardInner}>
@@ -27,7 +67,12 @@ const MemoryContainer = ({
           resizeMode={FastImage.resizeMode.cover}
           source={{ uri: item.memory_type == 'VIDEO' ? item?.thumbnail_memory_video_file : item?.memory_file }} />
         <View style={styles.cardBottomContent}>
-          <AppText style={styles.titleStyle}>{item?.challenge_details?.name}</AppText>
+          <View style={{ flexDirection: 'row', marginVertical: 7, alignItems: 'center', justifyContent: 'space-between' }}>
+            <AppText numberOfLines={1} style={styles.titleStyle}>{item?.challenge_details?.name}</AppText>
+            <TouchableOpacity onPress={saveToGallery} style={{ marginStart: 10 }}>
+              <DownloadImg style={{ width: 16, height: 16 }} />
+            </TouchableOpacity>
+          </View>
           <AppText numberOfLines={2} style={styles.Text}>{item?.challenge_details?.description.replace(/<[^>]+>/g, '')}</AppText>
         </View>
       </View>
