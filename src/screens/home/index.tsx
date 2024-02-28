9e1c35fb0b22e94b66e0e3851a7cd47af26dc209
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-02-28 16:54:46 +0500

    - Update Home Text for AR

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -137,7 +137,7 @@ const Home: ScreenStackComponent<RootStackParamList, "Home"> = ({ route }) => {
         <View style={styles.innerView}>
           <AppText style={styles.headerText}>{item?.title}</AppText>
           <AppText style={styles.headerText}>{item?.subtitle}</AppText>
-          <AppText style={styles.subtitleText}>Praesent et leo cursus, malesuada magna eu</AppText>
+          <AppText style={styles.subtitleText}>{item?.description}</AppText>
           <AppText style={styles.challengesText}>{numberOfChallenges} Challenges</AppText>
         </View>
         <TouchableOpacity onPress={item?.id === 1 ? navigateToARChanllenge : () => Alert.alert('InProgress')}>
