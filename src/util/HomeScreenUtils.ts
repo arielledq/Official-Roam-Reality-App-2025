965b78f9f19f308d296ca8fdf60beeb32c3cf477
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-02-28 16:54:46 +0500

    - Update Home Text for AR

--- src/util/HomeScreenUtils.ts
+++ src/util/HomeScreenUtils.ts
@@ -5,12 +5,14 @@ export const HomeScreenData = [
     id: 1,
     title: "Anywhere",
     subtitle: "AR Challenges",
-    image: Images.Home
+    image: Images.Home,
+    description: "These are AR challenges that you can do anytime & anywhere"
   },
   {
     id: 2,
     title: "Outdoor GEO-AR",
     subtitle: "Adventures",
-    image: Images.Home1
+    image: Images.Home1,
+    description: "Follow our digital AR stars to find the best sites & attractions nationwide."
   }
 ]
