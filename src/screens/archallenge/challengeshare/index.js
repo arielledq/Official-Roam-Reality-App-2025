f160bfe03e09067de6133559b67cabde615c2639
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-04-04 17:20:29 +0500

    - share in share challenge done

--- src/screens/archallenge/challengeshare/index.js
+++ src/screens/archallenge/challengeshare/index.js
@@ -260,7 +260,9 @@ const ArChallengeShare = ({
   }
 
   const checkPermission = () => {
-    CameraRoll.saveAsset(captureData, { type: fileExt == 'mp4' ? 'video' : "photo" })
+    CameraRoll.saveAsset(captureData, { type: fileExt == 'mp4' ? 'video' : "photo" }).then(()=>{
+      Alert.alert("Saved to Camera Roll.")
+    });
   };
 
 
@@ -291,14 +293,19 @@ const ArChallengeShare = ({
               <AppText style={styles.pointCount}>{challengeObj.points}</AppText>
               <AppText style={styles.pointCountText}>Points</AppText>
             </View>
-            <View style={{ paddingHorizontal: 10 }}>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <View style={{ paddingHorizontal: 10,flex:1 }}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%' }}>
                 <Image style={{ width: 24, height: 24, marginEnd: 10 }} source={{ uri: challengeObj.sponsored.image }} />
                 <Text style={styles.challengeSponsorName}>{challengeObj?.sponsored?.name}</Text>
               </View>
-              <View >
+              <View style={{width:'100%'}}>
                 <Text style={styles.challengeSponsorTipText}>Share your recorded experience for extra credits!</Text>
-                <Text style={styles.challengeSponsorStartDateText}>Completed on : {startDate}</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%',justifyContent:'space-between',marginTop:2}}>
+                  <Text style={styles.challengeSponsorStartDateText}>Completed on : {startDate}</Text>
+                  <TouchableOpacity onPress={checkPermission} style={styles.shareBtn}>
+                    <DownloadImg />
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </View>
@@ -313,9 +320,6 @@ const ArChallengeShare = ({
             </TouchableOpacity>
             <TouchableOpacity onPress={TiktokShareImgOnPress} style={styles.shareBtn}>
               <TiktokShareImg />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={checkPermission} style={styles.shareBtn}>
-              <DownloadImg />
             </TouchableOpacity>
           </View>
           <Text style={styles.shareText}>1 Extra Point Per Platform</Text>
