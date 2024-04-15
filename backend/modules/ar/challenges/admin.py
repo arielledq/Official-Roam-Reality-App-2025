112f1c062699c8722206311bad190f74fa356136
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-04-15 22:30:16 +0500

    - add google map

--- backend/modules/ar/challenges/admin.py
+++ backend/modules/ar/challenges/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Challenges, Sponsor, ARUserProfile, ARMemories, ARSettings, ARExample, GeoArChallenge
+from .widgets import GoogleMapsOpenLayersWidget
+from django.contrib.gis.db.models import MultiPolygonField
 
 class ARMemoriesAdmin(admin.ModelAdmin):
     pass
@@ -13,4 +15,10 @@ admin.site.register(ARUserProfile, ARChallengeAdmin)
 admin.site.register(ARMemories, ARMemoriesAdmin)
 admin.site.register(ARSettings, ARMemoriesAdmin)
 admin.site.register(ARExample, ARMemoriesAdmin)
-admin.site.register(GeoArChallenge, ARChallengeAdmin)
+
+# Register your models here.
+@admin.register(GeoArChallenge)
+class GeoArChallengeAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        MultiPolygonField: {"widget": GoogleMapsOpenLayersWidget}
+    }
