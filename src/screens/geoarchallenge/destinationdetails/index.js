5015d722830119c6e7a7055ab2dc390008d9410e
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-04-22 16:36:58 +0500

    - map working

--- src/screens/geoarchallenge/destinationdetails/index.js
+++ src/screens/geoarchallenge/destinationdetails/index.js
@@ -1,144 +1,49 @@
 import React, { useEffect, useState } from "react"
 
 import { ActivityIndicator, FlatList, Image, ImageBackground, Keyboard, Text, TouchableOpacity, View } from "react-native";
-import { handleError } from "../../util/helpers"
-import { getARChallenges, getARProfile, getARStettings } from '../../network'
-import BackgroundWithImage from "../../components/background"
-import AppHeader from "../../components/header"
+import { handleError } from "../../../util/helpers"
+import { getARChallenges, getARProfile, getARStettings } from '../../../network'
+import BackgroundWithImage from "../../../components/background"
+import AppHeader from "../../../components/header"
 import { useNavigation } from "@react-navigation/native"
-import SiteIcon from "../../assets/geoar/siteicon.svg"
-import StarSiteIcon from "../../assets/geoar/starsite.svg"
-import GradientDown from "../../assets/geoar/gradient_down.svg"
-import GradientDownPNG from "../../assets/geoar/gradient_down.png"
-import BellIcon from "../../assets/geoar/bell.svg"
-import LocationIcon from "../../assets/geoar/location.png"
-import BackImg from "../../assets/geoar/back_img.png"
-import ArIcon from "../../assets/geoar/aricon.svg"
-import { updateARUserData, updateARSettings } from "../../redux/AR"
+import SiteIcon from "../../../assets/geoar/siteicon.svg"
+import StarSiteIcon from "../../../assets/geoar/starsite.svg"
+import GradientDown from "../../../assets/geoar/gradient_down.svg"
+import GradientDownPNG from "../../../assets/geoar/gradient_down.png"
+import BellIcon from "../../../assets/geoar/bell.svg"
+import LocationIcon from "../../../assets/geoar/location.png"
+import BackImg from "../../../assets/geoar/back_img.png"
+import ArIcon from "../../../assets/geoar/aricon.svg"
+import { updateARUserData, updateARSettings } from "../../../redux/AR"
 
 import { useDispatch, useSelector } from "react-redux"
 import useStyles from "./styles"
 import LinearGradient from "react-native-linear-gradient";
-import { height, width } from "../../util/AppDimensions";
+import { height, width } from "../../../util/AppDimensions";
 
 
-const GeoArChallenge = ({
+const GeoArChallengeDetails = ({
 
 }) => {
   const _styles = useStyles()
   const dispatch = useDispatch()
   const [isLoading, setIsLoading] = useState(false)
-  const [challengeChoice, setChallengeChoice] = useState("SPONSORED")
-  const [sponsoredDataAll, setSponsoredDataAll] = useState([])
-  const [sponsoredData, setSponsoredData] = useState([])
-  const arProfile = useSelector(state => state.ar?.arProfile)
-  const navigation = useNavigation()
 
-  const ARSposored = () => {
-    setIsLoading(true)
-    getARChallenges().then((res) => {
-      if (res.status == 1) {
-        setSponsoredDataAll(res.data)
-        setSponsoredData(res.data.filter(x => x.challenge_choice == challengeChoice))
-      } else {
-        res.message.message = "Error in loading Challenges."
-        handleError(res)
-      }
-    }).finally(() => {
-      setIsLoading(false)
-    })
-  }
-
-  const ARUserProfile = () => {
-    setIsLoading(true)
-    getARProfile().then((res) => {
-      if (res.status == 1) {
-        dispatch(updateARUserData(res))
-      } else {
-        res.message.message = "Error in loading Challenges."
-        handleError(res)
-      }
-    }).finally(() => {
-      setIsLoading(false)
-    })
-  }
-
-  const getSettings = () => {
-    setIsLoading(true)
-    getARStettings().then((res) => {
-      if (res.data.length > 0) {
-        dispatch(updateARSettings(res.data[0]))
-      }
-    }).finally(() => {
-      setIsLoading(false)
-    })
-  }
-
-  const setDataWithChoice = (choice) => {
-    setChallengeChoice(choice);
-    const filteredArray = sponsoredDataAll.filter(x => x.challenge_choice == choice)
-    setSponsoredData(filteredArray.slice())
-  }
-
-  useEffect(() => {
-    ARSposored()
-    ARUserProfile()
-    getSettings()
-  }, []);
-
-  const navigateToChallengeDetails = (obj) => {
-    navigation.navigate("ArChallengeDetails", { challengeObj: obj });
-  }
-
-  const Item = ({ obj }) => (
-    <TouchableOpacity onPress={() => navigateToChallengeDetails(obj)} style={{width:'100%'}}>
-      <ImageBackground style={_styles.containerView} resizeMode="cover" source={BackImg}>
-        <Image source={GradientDownPNG} resizeMode="cover" style={{ position: 'absolute', bottom: 0, left:0, right: 0, top: 0,width:'110%' }} />
-        <View style={{ width: '100%', marginBottom: 10 }}>
-          <Text style={_styles.list_title}>Trinidad</Text>
-          <View style={{ flexDirection: 'row', justifyContent: "flex-start", width: '100%', alignItems: "flex-start", marginTop: 20 }}>
-            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <SiteIcon style={{ width: 48, height: 48 }} />
-              <Text style={_styles.s_list_count}>18+</Text>
-              <Text style={_styles.s_list_text}>Sites</Text>
-            </View>
-            <View style={{ alignItems: 'center', justifyContent: 'center', marginStart: 22, marginEnd: 10 }}>
-              <StarSiteIcon style={{ width: 48, height: 48 }} />
-              <Text style={_styles.s_list_count}>8+</Text>
-              <Text style={_styles.s_list_text}>Star Sites</Text>
-            </View>
-            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-              <ArIcon style={{ width: 48, height: 48 }} />
-              <Text style={_styles.s_list_count}>64+</Text>
-              <Text style={_styles.s_list_text}>AR Challenges</Text>
-            </View>
-          </View>
-        </View>
-      </ImageBackground>
-    </TouchableOpacity>
-  );
 
   return (
 
     <BackgroundWithImage style={_styles.mainContainer}>
       <AppHeader
         centerComponent={{
-          text: "AR Experiences",
+          text: "Trinidad",
           style: [_styles.heading],
         }} backgroundColor="transparent" />
 
       {isLoading && <ActivityIndicator size="large" />}
-      <FlatList
-        style={{ flex: 1, marginVertical: 15 }}
-        data={sponsoredData}
-        numColumns={1}
-        renderItem={({ item }) => <Item obj={item} />}
-        keyExtractor={item => item.id}
-      />
     </BackgroundWithImage >
   )
 }
 
 
 
-export default GeoArChallenge
+export default GeoArChallengeDetails
