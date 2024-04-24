784964a0bfd4c0abc5d0e7aabf2795d2f80cc77d
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-04-24 20:59:11 +0500

    - Sites details UI

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -34,6 +34,7 @@ import ContactUs from '../screens/ContactUs/ContactUs'
 import FAQ from '../screens/FAQ/FAQ';
 import GeoArChallenge from '../screens/geoarchallenge';
 import GeoArChallengeDetails from '../screens/geoarchallenge/destinationdetails';
+import GeoArSiteDetails from '../screens/geoarchallenge/sitedetails';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const Drawer = createDrawerNavigator();
@@ -94,6 +95,7 @@ const Navigation = () => {
         <Stack.Screen name="ArChallengeShare" component={ArChallengeShare} />
         <Stack.Screen name="GeoArChallenge" component={GeoArChallenge} />
         <Stack.Screen name="GeoArChallengeDetails" component={GeoArChallengeDetails} />
+        <Stack.Screen name="GeoArSiteDetails" component={GeoArSiteDetails} />
         <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="Privacy" component={Privacy} />
         <Stack.Screen name="EmailVerificationC" component={EmailVerification} />
--- src/screens/geoarchallenge/destinationdetails/index.js
+++ src/screens/geoarchallenge/destinationdetails/index.js
@@ -17,12 +17,10 @@ import ArIcon from "../../../assets/geoar/aricon.svg"
 import PinIcon from "../../../assets/geoar/pinicon.svg"
 import SitesIcon from "../../../assets/geoar/sites.svg"
 import Map from "../../../assets/geoar/map.png"
-import { updateARUserData, updateARSettings } from "../../../redux/AR"
+import MapView from 'react-native-maps';
 
 import { useDispatch, useSelector } from "react-redux"
 import useStyles from "./styles"
-import LinearGradient from "react-native-linear-gradient";
-import { height, width } from "../../../util/AppDimensions";
 
 
 const GeoArChallengeDetails = ({
@@ -31,6 +29,7 @@ const GeoArChallengeDetails = ({
   const _styles = useStyles()
   const dispatch = useDispatch()
   const [isLoading, setIsLoading] = useState(false)
+  const navigation = useNavigation()
 
 
   return (
@@ -43,12 +42,12 @@ const GeoArChallengeDetails = ({
         }} backgroundColor="transparent" />
 
       {isLoading && <ActivityIndicator size="large" />}
-      <View style={{marginVertical:20}}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false} style={{ width: '100%',height: 50 }} contentContainerStyle={_styles.rowView}>
+      <View style={{ marginVertical: 20 }}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false} style={{ width: '100%', height: 50 }} contentContainerStyle={_styles.rowView}>
           <TouchableOpacity activeOpacity={.5} style={_styles.selectButtonStyle}>
             <Text style={_styles.buttonSelectText}>Full</Text>
           </TouchableOpacity>
-          <TouchableOpacity activeOpacity={.5} style={_styles.unSelectButtonStyle}>
+          <TouchableOpacity onPress={()=>navigation.navigate("GeoArSiteDetails")} activeOpacity={.5} style={_styles.unSelectButtonStyle}>
             <Text style={_styles.buttonSelectText}>Diego Martin Region</Text>
           </TouchableOpacity>
           <TouchableOpacity activeOpacity={.5} style={_styles.unSelectButtonStyle}>
@@ -56,20 +55,28 @@ const GeoArChallengeDetails = ({
           </TouchableOpacity>
         </ScrollView>
       </View>
-      <View>
-        <Image source={Map} />
+      <View style={{ width: '100%',position:'relative',flex:1, borderRadius:16, overflow:'hidden'}}>
+        <MapView
+          style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
-      <View style={{ flexDirection: 'row', justifyContent: "space-between", width: '100%', alignItems: "flex-start", marginTop: 20 }}>
+      <View style={{ flexDirection: 'row', justifyContent: "space-between", width: '100%', alignItems: "flex-start", marginTop: 20,marginBottom:30 }}>
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
           <SiteIcon style={{ width: 48, height: 48 }} />
           <Text style={_styles.s_list_count}>18+</Text>
           <Text style={_styles.s_list_text}>Sites</Text>
         </View>
-        <View style={{ alignItems: 'center', justifyContent: 'center', marginStart: 22, marginEnd: 10 }}>
+        <View style={{ alignItems: 'center', justifyContent: 'center'}}>
           <SitesIcon style={{ width: 48, height: 48 }} />
           <Text style={_styles.s_list_count}>8+</Text>
           <Text style={_styles.s_list_text}>Star Sites</Text>
-        </View><View style={{ alignItems: 'center', justifyContent: 'center', marginStart: 22, marginEnd: 10 }}>
+        </View><View style={{ alignItems: 'center', justifyContent: 'center' }}>
           <StarSiteIcon style={{ width: 48, height: 48 }} />
           <Text style={_styles.s_list_count}>100+</Text>
           <Text style={_styles.s_list_text}>Hidden Sites</Text>
