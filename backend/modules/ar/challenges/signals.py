98085f214353e98de933e997f23d1afd7a426019
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-05-17 01:59:06 +0500

    - updated models

--- backend/modules/ar/challenges/signals.py
+++ backend/modules/ar/challenges/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Challenges, Sponsor, Resource3dModel, ARUserProfile, ARMemories
+from .models import Challenges, Sponsor, ARUserProfile, ARMemories
 from django.db.models import F
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.files.base import File
--- backend/modules/ar/challenges/urls.py
+++ backend/modules/ar/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 from . import signals  # noqa
 from .viewsets import ChallengesViewSet, ChallengesUploadView,\
-    SponsorViewSet, Resource3dModelViewSet, ARProfileViewSet, ARMemoriesViewSet, ARSettingsViewSet, \
+    SponsorViewSet, ARProfileViewSet, ARMemoriesViewSet, ARSettingsViewSet, \
     ARExamplesViewSet, GeoArSiteViewSet, GeoLocationViewSet
 
 router = routers.DefaultRouter()
@@ -13,7 +13,6 @@ router.register(r'sponsor', SponsorViewSet)
 router.register(r'memories', ARMemoriesViewSet, basename="ar-memories")
 router.register(r'settings', ARSettingsViewSet)
 router.register(r'examples', ARExamplesViewSet)
-router.register(r'resource', Resource3dModelViewSet)
 router.register(r'geo-ar-location', GeoLocationViewSet)
 router.register(r'geo-ar-site', GeoArSiteViewSet)
 
