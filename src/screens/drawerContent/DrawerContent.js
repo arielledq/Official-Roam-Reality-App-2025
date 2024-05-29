701de42e4c9cff41e0d6334d96821781bba8e497
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-05-29 14:42:15 +0500

    Update DrawerContent.js

--- src/screens/drawerContent/DrawerContent.js
+++ src/screens/drawerContent/DrawerContent.js
@@ -16,7 +16,7 @@ import { GoogleSignin } from "@react-native-google-signin/google-signin"
 import { removeItem } from "../../util/helpers"
 
 const DrawerList = [
-  { icon: 'target', label: 'AR Challenges', navigateTo: 'GeoArChallenge' },
+  { icon: 'target', label: 'AR Challenges', navigateTo: 'Home' },
   { icon: "message-square", label: "Chats", navigateTo: "Home" },
   { icon: "users", label: "Friends", navigateTo: "Home" },
   { icon: "Wallet", label: "Wallet", navigateTo: "Home" },
