dac2e118ac2ff6bfb76e106972f9b96359dd2c25
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-06-11 18:32:09 +0500

    - pin checkins

--- backend/modules/ar/challenges/admin.py
+++ backend/modules/ar/challenges/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Challenges, Sponsor, ARUserProfile, ARMemories, ARSettings, ARExample, GeoArSite, GeoLocation, GeoARStar, \
-  ARChallengeParameterSettings, ARChallengeFilters, UniqueChallengeSite, GeoARChallenges, GeoRegion,GeoARSiteActivity,StarCollection
+  ARChallengeParameterSettings, ARChallengeFilters, UniqueChallengeSite, GeoARChallenges, GeoRegion,GeoARSiteActivity,StarCollection,ARSitePinCheckIn
 from .widgets import GoogleMapsOpenLayersWidget
 from django.contrib.gis.db.models import MultiPolygonField, PointField
 from django.contrib.gis.admin import OSMGeoAdmin
@@ -69,6 +69,19 @@ class GeoLocationAdmin(GeoArChallengeAdmin):
     list_display = ('name',"check_ins",)
     ordering = ("name","check_ins",)
     search_fields = ["name"]
+
+@admin.register(ARSitePinCheckIn)
+class ARSitePinCheckInAdmin(admin.ModelAdmin):
+    search_fields = (
+        "user__name",
+        "geo_site__name",
+    )
+    list_display = ('user_name', 'geo_site', 'approval','check_in_image')
+    list_select_related = ['user']  # To avoid extra queries
+
+    def user_name(self, obj):
+        return obj.user.name
+    pass
 
 admin.site.register(Sponsor, ARChallengeAdmin)
 admin.site.register(ARUserProfile, ARChallengeAdmin)
