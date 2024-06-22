67a1ef7b65a6e483adcdb775107d0fb8ae07fae4
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-06-22 20:21:51 +0500

    Update index.js

--- src/screens/archallenge/FilterView/index.js
+++ src/screens/archallenge/FilterView/index.js
@@ -12,6 +12,7 @@ import PagerView from 'react-native-pager-view';
 import { DragTextEditor } from 'react-native-drag-text-editor';
 import Geocoder from 'react-native-geocoding';
 import RightArrowIcon from "../../../assets/svg/RightArrowIcon"
+import { Image } from "@rneui/base";
 
 Geocoder.init("AIzaSyAd_EZRrfSjO2OS6p-h89wrT3y8xyREpTA");
 
@@ -126,17 +127,22 @@ const ARFilter = ({
     <View style={styles.mainContainer}>
       <ViewShot ref={viewShotRef} style={styles.mainContainer} options={{ fileName: "filtered_share", format: "jpg", quality: 0.9 }}>
         <BackgroundWithImage source={{ uri: captureData }} style={styles.mainContainer}>
+
           <PagerView style={styles.pagerView} initialPage={0}>
             {
               ar_filters.map((filter) => {
                 return (
                   <View key={filter?.id} style={{ position: 'relative', flex: 1 }}>
+
                     <LinearGradient style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
                       colors={
                         filter.gradient_direction == 'TOP_TO_BOTTOM' ?
                           [...filter.gradient_colors, 'transparent'] :
                           ['transparent', ...filter.gradient_colors]
                       } />
+                    {filter.image &&
+                      <Image source={filter.image} resizeMode="cover" style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, width: '100%', height: '100%' }} />
+                    }
                     {fullLocation &&
                       <View style={[styles.locationTextView, filter.gradient_direction == 'TOP_TO_BOTTOM' ? styles.locationTextTop : styles.locationTextBottom]}>
                         <View style={{ flex: 1, height: 2, backgroundColor: '#fff' }} />
@@ -157,9 +163,9 @@ const ARFilter = ({
 
       <View style={{
         position: 'absolute', bottom: 20, flex: 1, zIndex: 500, justifyContent: 'center', left: 0, right: 0, alignItems: 'flex-end',
-        padding: 20,justifyContent:'space-between',flexDirection:'row'
+        padding: 20, justifyContent: 'space-between', flexDirection: 'row'
       }}>
-        <TouchableOpacity  style={{transform: [{ rotate: '180deg' }]}} onPress={()=>navigation.goBack()}>
+        <TouchableOpacity style={{ transform: [{ rotate: '180deg' }] }} onPress={() => navigation.goBack()}>
           <RightArrowIcon />
         </TouchableOpacity>
         <TouchableOpacity onPress={navigateToShare}>
