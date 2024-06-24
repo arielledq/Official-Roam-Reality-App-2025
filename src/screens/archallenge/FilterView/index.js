b9dfdfad7ab6b9d1180a38413b24cade88338c3e
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-06-24 18:28:19 +0500

    - filter work done

--- src/screens/archallenge/FilterView/index.js
+++ src/screens/archallenge/FilterView/index.js
@@ -13,6 +13,7 @@ import { DragTextEditor } from 'react-native-drag-text-editor';
 import Geocoder from 'react-native-geocoding';
 import RightArrowIcon from "../../../assets/svg/RightArrowIcon"
 import { Image } from "@rneui/base";
+import { moderateScale } from "../../../util/AppDimensions";
 
 Geocoder.init("AIzaSyAd_EZRrfSjO2OS6p-h89wrT3y8xyREpTA");
 
@@ -27,6 +28,7 @@ const ARFilter = ({
   const ar_filters = challengeObj?.ar_filters;
   const [location, setLocation] = useState(null)
   const [fullLocation, setFullLocation] = useState(null)
+  const [imageHeight, setImageHeight] = useState(0)
 
   const viewComponent = () => <View style={styles.cornerStyles} />;
 
@@ -118,6 +120,13 @@ const ARFilter = ({
 
   useEffect(() => {
     getLocation()
+    Image.getSize(captureData, (width, height) => {
+      // calculate image width and height 
+      const screenWidth = Dimensions.get('window').width - (2 * moderateScale(26))
+      const scaleFactor = width / screenWidth
+      const imageHeight = height / scaleFactor
+      setImageHeight(imageHeight)
+    })
   }, []);
 
   return (
@@ -126,28 +135,34 @@ const ARFilter = ({
         <PagerView style={styles.pagerView} initialPage={0}>
           {
             ar_filters.map((filter) => {
+              console.log(filter.image)
               return (
                 <View key={filter?.id} style={{ position: 'relative', flex: 1 }}>
-
-                  <LinearGradient style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
-                    colors={
-                      filter.gradient_direction == 'TOP_TO_BOTTOM' ?
-                        [...filter.gradient_colors, 'transparent'] :
-                        ['transparent', ...filter.gradient_colors]
-                    } />
-                  {filter.image &&
-                    <Image source={filter.image} resizeMode="cover" style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, width: '100%', height: '100%' }} />
+                  {filter.image ?
+                    <Image source={{ uri: filter.image }} resizeMode="cover" style={{ height: imageHeight, width: '100%' }} />
+                    : <LinearGradient style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
+                      colors={
+                        filter.gradient_direction == 'TOP_TO_BOTTOM' ?
+                          [...filter.gradient_colors, 'transparent'] :
+                          ['transparent', ...filter.gradient_colors]
+                      } />
                   }
-                  {fullLocation &&
-                    <View style={[styles.locationTextView, filter.gradient_direction == 'TOP_TO_BOTTOM' ? styles.locationTextTop : styles.locationTextBottom]}>
-                      {/* <View style={{ flex: 1, height: 2, backgroundColor: '#fff' }} /> */}
+                  <View style={[styles.textFilterView, { justifyContent: filter.gradient_direction == 'TOP_TO_BOTTOM' ? "flex-start" : "flex-end" }]}>
+                    {fullLocation &&
                       <Text style={[styles.locationText,
                       { color: filter.location_text_color, fontSize: Number(filter.location_text_size) }]}>{getLocationText(filter.location_option)}</Text>
-                      {/* <View style={{ flex: 1, height: 2, backgroundColor: '#fff' }} /> */}
-                    </View>}
-                  <View style={[styles.filterTextView, filter.gradient_direction == 'TOP_TO_BOTTOM' ? styles.filterTextTop : styles.filterTextBottom]}>
-                    <Text style={[styles.bottomText, { color: filter.filter_text_color, fontSize: Number(filter.filter_text_size) }]}>{filter.filter_text}</Text>
+                    }
+                    {!filter.text_form_image &&
+                      <Text
+                        style={[styles.bottomText,
+                        { color: filter.filter_text_color, fontSize: Number(filter.filter_text_size) }]}>{filter.filter_text}</Text>
+                    }
+                    {!
+                      filter.text_form_image &&
+                      <Text style={[styles.appNameText,
+                      { color: filter.app_name_text_color, fontSize: Number(filter.app_name_text_size) }]}>{filter.app_name_text}</Text>}
                   </View>
+
                 </View>
               )
             })
