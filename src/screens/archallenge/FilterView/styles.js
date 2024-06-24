16b93fb126d726fc6a0d28e1cad94c1f8e13795f
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-06-24 18:28:19 +0500

    - filter work done

--- src/screens/archallenge/FilterView/styles.js
+++ src/screens/archallenge/FilterView/styles.js
@@ -9,7 +9,8 @@ import { FontFamily, FontLineHeights, FontSizes, fontGroup } from "../../../util
 
 const useStyles = makeStyles(theme => ({
   mainContainer: {
-    flex: 1
+    flex: 1,
+    backgroundColor:'transparent'
   },
   buttonStyle: {
     height: 50
@@ -52,29 +53,29 @@ const useStyles = makeStyles(theme => ({
     fontSize: FontSizes.S22,
     lineHeight: FontLineHeights.LH25,
     marginHorizontal: 8,
-    maxWidth:'75%',
-    textAlign:'center',
-    ...fontGroup.giItalic,
+    maxWidth: '75%',
+    textAlign: 'center',
+    ...fontGroup.giRegular,
   },
   bottomText: {
     color: '#fff',
     fontSize: FontSizes.S30,
     lineHeight: FontLineHeights.LH33,
-    ...fontGroup.giBold,
+    ...fontGroup.giItalic,
     marginHorizontal: 5,
-    textAlign:'center'
+    textAlign: 'center'
   },
-  filterTextView: { 
-    position: 'absolute', 
-    zIndex: 20, 
-    right: 0, 
-    left: 0, 
-    alignItems: 'center' 
+  filterTextView: {
+    position: 'absolute',
+    zIndex: 20,
+    right: 0,
+    left: 0,
+    alignItems: 'center'
   },
-  locationTextView: { 
-    position: 'absolute', 
-    right: 0, 
-    left: 0, 
+  locationTextView: {
+    position: 'absolute',
+    right: 0,
+    left: 0,
     alignItems: 'center',
   },
   filterTextBottom: {
@@ -89,6 +90,30 @@ const useStyles = makeStyles(theme => ({
   locationTextTop: {
     top: 25
   },
+  appNameText: {
+    color: '#fff',
+    fontSize: FontSizes.S14,
+    lineHeight: FontLineHeights.LH33,
+    ...fontGroup.giBold,
+    marginHorizontal: 5,
+    textAlign: 'center'
+  },
+  textFilterView: {
+    justifyContent: "flex-start",
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    paddingVertical:10
+
+  },
+  imageFilterView: {
+    width:100,height:100
+  }
 }))
 
 export default useStyles
