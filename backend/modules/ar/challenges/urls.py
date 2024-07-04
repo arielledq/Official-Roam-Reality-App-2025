8e82b4ec426e13ace97e55a917ecd2d425596bf1
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-07-04 15:48:15 +0500

    - update model for ar parameters and panic message

--- backend/modules/ar/challenges/urls.py
+++ backend/modules/ar/challenges/urls.py
@@ -5,7 +5,7 @@ from . import signals  # noqa
 from .viewsets import ChallengesViewSet, ChallengesUploadView,\
     SponsorViewSet, ARProfileViewSet, ARMemoriesViewSet, ARSettingsViewSet, \
     ARExamplesViewSet, GeoArSiteViewSet, GeoLocationViewSet, GeoArStarViewSet, ARSitePinCheckInViewSet, \
-    StarCollectionViewSet, GoldStarCollectionViewSet, DestinationFactsViewSet
+    StarCollectionViewSet, GoldStarCollectionViewSet, DestinationFactsViewSet, PanicMessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'user', ChallengesViewSet)
@@ -21,6 +21,7 @@ router.register(r'geo-ar-star', GeoArStarViewSet, basename="ar-stars")
 router.register(r'geo-ar-star-collect', StarCollectionViewSet, basename="ar-stars")
 router.register(r'geo-ar-gold-star', GoldStarCollectionViewSet, basename="ar-gold-stars")
 router.register(r'geo-destination-fact', DestinationFactsViewSet, basename="geo-destination-fact")
+router.register(r'panic-message', PanicMessageViewSet, basename="panic-message")
 
 urlpatterns = [
     path('', include(router.urls)),
