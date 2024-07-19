8e9bc3f7d58c478a7fd811016eb1fec4fd99c354
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-07-19 19:23:48 +0500

    Update index.js

--- src/screens/archallenge/challengecapture/index.js
+++ src/screens/archallenge/challengecapture/index.js
@@ -61,6 +61,7 @@ const ArChallengeCapture = ({
   }
 
   const ARScreen = () => {
+    const [object3dType, setObject3dType] = useState("VRX");
     const [modelPath, setModelPath] = useState(null);
     const [sourcesFiles, setSourcesFiles] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -110,9 +111,14 @@ const ArChallengeCapture = ({
               for (let i = 0; i < result.length; i++) {
                 if (result[i].isFile) {
                   console.log("unzipModelFile", result[i].name)
-                  if (result[i].name.includes(".vrx")) {
+                  if (result[i].name.includes(".vrx") || result[i].name.includes(".VRX")) {
                     const vrxFile = Platform.OS === 'android' ? `file://${result[i].path}` : result[i].path
+                    setObject3dType("VRX")
                     setModelPath(vrxFile)
+                  } else if (result[i].name.includes(".obj") || result[i].name.includes(".OBJ")) {
+                    const objFile = Platform.OS === 'android' ? `file://${result[i].path}` : result[i].path
+                    setModelPath(objFile)
+                    setObject3dType("OBJ")
                   } else {
                     const sourceFile = Platform.OS === 'android' ? `file://${result[i].path}` : result[i].path
                     sourcesArray.push(sourceFile)
@@ -240,7 +246,7 @@ const ArChallengeCapture = ({
             challengeObjParameters?.positionY ? Number(challengeObjParameters?.positionY) : -5,
             challengeObjParameters?.positionZ ? Number(challengeObjParameters?.positionZ) : -25]}
             scale={scale}
-            type="VRX"
+            type={object3dType}
             opacity={challengeObjParameters?.image_opacity ? Number(challengeObjParameters?.image_opacity_value) : 1}
             materials={challengeObjParameters?.bloom ? ["mat"] : ["grid"]}
             rotation={rotate}
