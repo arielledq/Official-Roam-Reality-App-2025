7952fdac5f31672252e403c10b80d36d1ec3771c
Shabeeb Hasan <shabeebhasan@users.noreply.github.com>
2024-07-23 16:45:25 +0500

    Update index.js

--- src/screens/archallenge/challengeshare/index.js
+++ src/screens/archallenge/challengeshare/index.js
@@ -330,9 +330,10 @@ const ArChallengeShare = ({
             <TouchableOpacity onPress={InstagramShareImgOnPress} style={styles.shareBtn}>
               <InstagramShareImg />
             </TouchableOpacity>
-            <TouchableOpacity onPress={TiktokShareImgOnPress} style={styles.shareBtn}>
+            {fileExt == 'mp4' && <TouchableOpacity onPress={TiktokShareImgOnPress} style={styles.shareBtn}>
               <TiktokShareImg />
             </TouchableOpacity>
+            }
           </View>
           <Text style={styles.shareText}>1 Extra Point Per Platform</Text>
         </View>
