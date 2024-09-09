c555b4fc8fc0f30d49d63c2e86abd048891cf544
Rafael Bastos <clemente.rafael@crodbotics.com>
2024-09-09 18:11:16 -0400

    add migrations

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -5,14 +5,14 @@ from notifications.models import NotificationError, Notification
 
 
 @admin.register(NotificationError)
-class NotificationErrorAdmin(NotificationError):
+class NotificationErrorAdmin(admin.ModelAdmin):
     list_display = ('id', 'message')
     ordering = ("id",)
     search_fields = ["id", "message"]
 
 
 @admin.register(Notification)
-class NotificationAdmin(Notification):
+class NotificationAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     ordering = ("id",)
     search_fields = ["id", "title"]
