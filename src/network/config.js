561ce103150857f2173b38c3e6f4c7b51c8b5051
Rafael Bastos <clemente.rafael@crodbotics.com>
2024-09-09 14:45:59 -0400

    setup onesignal app package

--- src/network/config.js
+++ src/network/config.js
@@ -1,18 +1,17 @@
-import { store } from '../store/index'
+import Config from "../config"
+import { store } from "../store/index"
 
-export const BASE_URL = 'https://travel-ar-app-42706-staging.botics.co/'
+export const BASE_URL = Config.BASE_URL
+export const APPLE_CLIENT_ID = Config.APPLE_CLIENT_ID
+export const APPLE_REDIRECT_URL = Config.APPLE_CLIENT_ID
 
-export const commonApiRoute = 'api/v1/'
-
-export const APPLE_CLIENT_ID = 'com.roam.reality.services'
-export const APPLE_REDIRECT_URL =
-  'https://travel-ar-app-42706-staging.botics.co/accounts/apple/login/callback/'
+export const commonApiRoute = "api/v1/"
 
 export const MULTIPART_HEADER = async () => {
   const token = await store.getState().login.data.token
   const header = {
-    Accept: 'application/json',
-    'Content-Type': 'multipart/form-data'
+    Accept: "application/json",
+    "Content-Type": "multipart/form-data"
   }
   if (token) {
     header.Authorization = `Token ${token}`
@@ -21,16 +20,16 @@ export const MULTIPART_HEADER = async () => {
 }
 
 export const APP_JSON_HEADER = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json'
+  Accept: "application/json",
+  "Content-Type": "application/json"
 }
 
 export const TOKEN_HEADER = async () => {
   const token = await store.getState().login.data.token
   console.log({ token })
   const header = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    Accept: "application/json",
+    "Content-Type": "application/json",
     Authorization: `Token ${token}`
   }
   return header
