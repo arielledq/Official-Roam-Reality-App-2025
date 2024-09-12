c584468c4d9c2ea852c8b3441c03054e6879a5f4
Rafael Bastos <clemente.rafael@crodbotics.com>
2024-09-12 10:20:21 -0400

    print

--- backend/onesignal_client/models.py
+++ backend/onesignal_client/models.py
@@ -21,6 +21,8 @@ class UserDevice(models.Model):
 
     @staticmethod
     def activate_device(user, device_id, device_token):
+        print('device_id')
+        print(device_id)
         if not device_id:
             return
         try:
