c249be3c1963f4b3ab8e1d4860f980312440fcad
hbittar <hassankarimbittar@gmail.com>
2024-09-26 16:41:32 -0400

    added action get_all to pull all the DestinationFacts

--- backend/modules/ar/challenges/viewsets.py
+++ backend/modules/ar/challenges/viewsets.py
@@ -509,3 +509,9 @@ class DestinationFactsViewSet(ViewSet):
         objs = self.queryset.filter(criterion)
         serializer = DestinationFactsSerializer(objs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'])
+    def get_all(self, request):
+        objs = self.queryset.all()
+        serializer = DestinationFactsSerializer(objs, many=True)
+        return Response(serializer.data)
