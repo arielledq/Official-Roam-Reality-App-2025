977440df45c48e417c3f7af700091cc18044bce3
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-10-04 17:37:55 -0400

    removed test user

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -86,8 +86,8 @@ const Login: ScreenStackComponent<RootStackParamList, 'Login'> = ({ navigation }
       >
         <Formik
           initialValues={{
-            email: 'user01@yopmail.com',
-            password: 'Password123@',
+            email: '',
+            password: '',
           }}
           onSubmit={v => handleLogin(v)}
           validationSchema={SigninSchema}
