06aad882156ef5510622f538c13523eb5f7188b7
hbittar <hassankarimbittar@gmail.com>
2024-10-16 15:35:42 -0300

    added mapbox

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -11,5 +11,6 @@ export default {
   APPLE_REDIRECT_URL: 'https://travel-ar-app-42706-staging.botics.co/accounts/apple/login/callback/',
   ONE_SIGNAL_APP_ID: "ceda5dd2-8fd2-4180-9532-7b7f127a612d",
   SENTRY_DSN: "https://8a0ac5dac07d4ed0b64bd75ab3c5a765@sentry.innovatica.com.py//58",
-  GEOCODER_API_KEY: "AIzaSyAd_EZRrfSjO2OS6p-h89wrT3y8xyREpTA"
+  GEOCODER_API_KEY: "AIzaSyAd_EZRrfSjO2OS6p-h89wrT3y8xyREpTA",
+  MAPBOX_PUBLIC_KEY: "pk.eyJ1IjoiaGJpdHRhcjIiLCJhIjoiY20yMHA5ajVrMGJ0bDJsb2oyeWkxZTlvbyJ9.AMxq1QZvEvEnydL_LIZnxw"
 }
