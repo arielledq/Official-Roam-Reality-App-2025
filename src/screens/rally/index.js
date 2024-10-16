1bb21b637a48d9b58d7da8f641eba5e4578dec86
victor <victor.aguilera@innovatica.com.py>
2024-10-16 16:28:28 -0300

    Unity test - recibe parametros del backend

--- src/screens/rally/index.js
+++ src/screens/rally/index.js
@@ -1,69 +1,83 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { StyleSheet, View, Button, TextInput, ImageBackground } from "react-native";
+import { StyleSheet, View, Button, TextInput, Text, ImageBackground, TouchableOpacity } from "react-native";
 import UnityView from "@azesmway/react-native-unity";
-import { unzip } from 'react-native-zip-archive'
+import { unzip } from 'react-native-zip-archive';
+import Share from 'react-native-share';
 import RNFetchBlob from "rn-fetch-blob";
 import { useFocusEffect, useRoute } from "@react-navigation/native";
 import { getARChallenges } from "../../network";
 import { handleError } from "../../util/helpers";
-const { config, fs } = RNFetchBlob
-
+const { config, fs } = RNFetchBlob;
 
 const Rally = ({}) => {
-  const RNFS = require('react-native-fs')
-  const [threshold, setThreshold] = useState('');
-  const [intensity, setIntensity] = useState('');
-  const [object3dType, setObject3dType] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [modelPath, setModelPath] = useState(null)
-  const [sourcesFiles, setSourcesFiles] = useState([])
+  const RNFS = require('react-native-fs');
+  const [object3dType, setObject3dType] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [modelPath, setModelPath] = useState(null);
+  const [sourcesFiles, setSourcesFiles] = useState([]);
   const unityRef = useRef(null);
-  const route = useRoute()
-  const [challengeObj, setChallengeObj] = useState(null)
-  const [progress, setProgress] = useState([0, 0, 0])
-  const [sponsoredDataAll, setSponsoredDataAll] = useState([])
-  const [sponsoredData, setSponsoredData] = useState([])
-  let [modelOBJ, setModelOBJ] = useState(null);        // Para guardar el archivo .obj
-  let [modelResource, setModelResource] = useState(null); // Para guardar el archivo .mtl
-  let [modelTextures, setModelTextures] = useState([]);   // Para guardar las texturas (.jpg, .png)
+  const route = useRoute();
+  const [challengeObj, setChallengeObj] = useState(null);
+  const challengeParams = challengeObj?.parameters;
+  const [progress, setProgress] = useState([0, 0, 0]);
+  const [sponsoredData, setSponsoredData] = useState([]);
+  const [fileFound, setFileFound] = useState(null);
+  const [captureData, setCaptureData] = useState('');
+  let [modelOBJ, setModelOBJ] = useState(null);
+  let [modelResource, setModelResource] = useState(null);
+  let [textureBase, setTextureBase] = useState(null);
+  let [textureEmission, setTextureEmission] = useState(null);
   let [foldefile, setFoldefile] = useState([]);
-  // const challengeObj = route?.params?.challengeObj
   const modelFile = challengeObj?.model_file
+  
+  // Inicializamos los estados con valores predeterminados
+  const [threshold, setThreshold] = useState(0);
+  const [intensity, setIntensity] = useState(1);
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
+  const [scale, setScale] = useState({ x: 1, y: 1, z: 1 });
+  const [rotation, setRotation] = useState({ x: 0, y: 0, z: 0 });
+  const [emissionValue, setEmissionValue] = useState(1);
+
+  // Actualizamos los estados cuando challengeParams esté disponible
+  useEffect(() => {
+    if (challengeParams) {
+      setThreshold(challengeParams?.bloom_threshold || 0);
+      setIntensity(challengeParams?.image_opacity_value || 1);
+      setPosition({
+        x: challengeParams?.positionX || 0,
+        y: challengeParams?.positionY || 0,
+        z: challengeParams?.positionZ || 0,
+      });
+      setScale({
+        x: challengeParams?.scale_object || 1,
+        y: challengeParams?.scale_object || 1,
+        z: challengeParams?.scale_object || 1,
+      });
+      setEmissionValue(challengeParams?.diffuse_intensity || 1);
+    }
+  }, [challengeParams]);
 
   const ARSposored = () => {
     getARChallenges()
       .then(res => {
-        if (res.status == 1) {
-          // console.log(" obj ===>>>> ", JSON.stringify(res.data.filter(x => x.challenge_requirement == 'PHOTO'), null, 2))
-
-          // setSponsoredDataAll(res.data)
-          const obj = res.data.filter(x => x.id == 289)
-          setSponsoredData(obj)
-          setChallengeObj(obj[0])
+        if (res.status === 1) {
+          const obj = res.data.filter(x => x.id === 289);
+          setSponsoredData(obj);
+          setChallengeObj(obj[0]);
         } else {
-          res.message.message = 'Error in loading Challenges.'
-          handleError(res)
+          handleError(res);
         }
       })
-  }
-  // useEffect(() => {
-  //   if (sponsoredData.length > 0) {
-  //     setChallengeObj(sponsoredData[0]);  // Obtener todo el challengeObj
-  //   }
-  // }, [sponsoredData]);
-  //
-  // useEffect(() => {
-  //   if (challengeObj && challengeObj.model_file) {
-  //     checkIfModelExist();  // Verificar model_file una vez que challengeObj esté listo
-  //   }
-  // }, [challengeObj]);
-  //
-  // useEffect(()=> {
-  //   if (sponsoredData.length > 0) {
-  //     setChallengeObj(sponsoredData[0])
-  //   }
-  //    },[sponsoredData])
+      .catch(error => {
+        console.error("Error en la obtención de datos: ", error);
+      });
+  };
 
+  useFocusEffect(
+    useCallback(() => {
+      ARSposored();
+    }, [])
+  );
 
   const downloadModelFile = (sourcePath, targetPath) => {
     config({
@@ -72,18 +86,15 @@ const Rally = ({}) => {
     })
       .fetch('GET', modelFile)
       .progress((received, total) => {
-        // console.log("progress", received / total)
-        setProgress(Math.trunc(Number((received / total) * 100)))
+        setProgress(Math.trunc(Number((received / total) * 100)));
       })
       .then(res => {
-        // the temp file path
-        // console.log("The file saved to ", res.path())
-        unzipModelFile(res.path(), targetPath)
+        unzipModelFile(res.path(), targetPath);
       })
       .catch(error => {
-        console.error(error)
-      })
-  }
+        console.error(error);
+      });
+  };
 
   const unzipModelFile = (sourcePath, targetPath) => {
     const charset = 'UTF-8';
@@ -91,49 +102,44 @@ const Rally = ({}) => {
       .then(path => {
         RNFS.readDir(path).then(result => {
           const sourcesArray = [];
-          const texturesArray = []; // Array para almacenar texturas
           let objFile = null;
           let mtlFile = null;
+          let baseTexture = null;
+          let emissionTexture = null;
 
-          for (let i = 0; i < result.length; i++) {
-            if (result[i].isFile) {
-              const filePath = Platform.OS === 'android' ? `file://${result[i].path}` : result[i].path;
+          result.forEach(file => {
+            const filePath = Platform.OS === 'android' ? `file://${file.path}` : file.path;
 
-              if (result[i].name.includes('.vrx') || result[i].name.includes('.VRX')) {
-                setObject3dType('VRX');
-                setModelPath(filePath);
-              } else if (result[i].name.includes('.obj') || result[i].name.includes('.OBJ')) {
-                objFile = filePath; // Guardamos el archivo .obj
-              } else if (result[i].name.includes('.mtl') || result[i].name.includes('.MTL')) {
-                mtlFile = filePath; // Guardamos el archivo .mtl
-              } else if (result[i].name.includes('.jpg') || result[i].name.includes('.JPG') ||
-                result[i].name.includes('.png') || result[i].name.includes('.PNG')) {
-                texturesArray.push(filePath); // Guardamos las texturas en un array
-              } else if (result[i].name.includes('.glb') || result[i].name.includes('.GLB')) {
-                setObject3dType('GLB');
-                setModelPath(filePath);
-              } else if (result[i].name.includes('.gltf') || result[i].name.includes('.GLTF')) {
-                setObject3dType('GLTF');
-                setModelPath(filePath);
-              } else {
-                sourcesArray.push({ uri: filePath });
-              }
+            if (file.name.includes('.vrx')) {
+              setObject3dType('VRX');
+              setModelPath(filePath);
+            } else if (file.name.includes('.obj')) {
+              objFile = filePath;
+            } else if (file.name.includes('.mtl')) {
+              mtlFile = filePath;
+            } else if (file.name.toLowerCase().includes('diffuse')) {
+              baseTexture = filePath;
+            } else if (file.name.toLowerCase().includes('emission')) {
+              emissionTexture = filePath;
+            } else if (file.name.includes('.glb')) {
+              setObject3dType('GLB');
+              setModelPath(filePath);
+            } else if (file.name.includes('.gltf')) {
+              setObject3dType('GLTF');
+              setModelPath(filePath);
+            } else {
+              sourcesArray.push({ uri: filePath });
             }
-          }
+          });
 
-          // Actualizamos los estados con los archivos clasificados
           if (objFile) setModelOBJ(objFile);
           if (mtlFile) setModelResource(mtlFile);
-          if (texturesArray.length > 0) setModelTextures(texturesArray);
+          if (baseTexture) setTextureBase(baseTexture);
+          if (emissionTexture) setTextureEmission(emissionTexture);
           if (sourcesArray.length > 0) setSourcesFiles(sourcesArray);
 
           setFoldefile(result);
           setLoading(false);
-
-          console.log('Contenido descomprimido:', result);
-          console.log('OBJ file:', objFile);
-          console.log('MTL file:', mtlFile);
-          console.log('Textures:', texturesArray);
         });
       })
       .catch(err => {
@@ -141,20 +147,12 @@ const Rally = ({}) => {
       });
   };
 
-  // useEffect(() => {
-  //   if (sponsoredData.length > 0) {
-  //     setChallengeObj(sponsoredData[0]);  // Obtener todo ael challengeObj
-  //   }
-  //
-  // }, [sponsoredData]);
-  // console.log('SponsoredDATa', JSON.stringify(sponsoredData, null, 2));
   useEffect(() => {
     if (challengeObj && challengeObj.model_file) {
-      // Aquí solo ejecutamos si challengeObj no es null
       checkIfModelExist();
     }
   }, [challengeObj]);
-console.log(challengeObj)
+
   const checkIfModelExist = () => {
     if (challengeObj && challengeObj.model_file) {
       const modelFile = challengeObj.model_file;
@@ -178,59 +176,38 @@ console.log(challengeObj)
     }
   };
 
-  // useEffect(() => {
-  //   if (challengeObj && challengeObj.model_file) {
-  //     checkIfModelExist();
-  //   }
-  // }, [challengeObj]);
-  useFocusEffect(
-    useCallback(() => {
-      ARSposored();
-    }, [])
-  );
-
-  console.log("modelfile",modelFile)
-//UNITY//
-
-  const sendModelDataToUnity = () => {
-    if (unityRef.current && modelOBJ) {
-      const cleanObjFile = modelOBJ.replace('file://', '');  // Elimina el prefijo 'file://'
-      const cleanMtlFile = modelResource ? modelResource.replace('file://', '') : ''; // Si existe el archivo MTL
-      const textures = modelTextures ? modelTextures.map(tex => tex.replace('file://', '')) : []; // Texturas si existen
-
-      const modelData = {
-        objFile: cleanObjFile,         // Ruta al archivo OBJ sin 'file://'
-        mtlFile: cleanMtlFile,         // Ruta al archivo MTL (puede ser null)
-        textures: textures,            // Rutas a las texturas (puede ser un array vacío)
-      };
-      console.log("Enviando a Unity:", JSON.stringify(modelData));
-      console.log("OBJ file path (clean):", cleanObjFile);
-      if (unityRef.current) {
-        console.log("unityRef Encontrado");
-        unityRef.current.postMessage('Scripts', 'LoadModelFromReact', JSON.stringify(modelData));
-      }else{
-        console.log("unityRef NO Encontrado");
-      }
-    }
-  };
-
   const sendModelDataToUnitySpawn = () => {
-    if (unityRef.current && modelOBJ) {
-      const cleanObjFile = modelOBJ.replace('file://', '');
-      const cleanMtlFile = modelResource ? modelResource.replace('file://', '') : null;
-      const textures = modelTextures ? modelTextures.map(tex => tex.replace('file://', '')) : [];
 
+    if (unityRef.current && modelOBJ) {
       const modelData = {
-        objFile: cleanObjFile,
-        mtlFile: cleanMtlFile,
-        textures: textures,
+        objFile: modelOBJ.replace('file://', ''),
+        mtlFile: modelResource ? modelResource.replace('file://', '') : null,
+        textureBase: textureBase ? textureBase.replace('file://', '') : '',
+        textureEmission: textureEmission ? textureEmission.replace('file://', '') : '',
+        position: {
+          x: parseFloat(position.x) || 0,
+          y: parseFloat(position.y) || 0,
+          z: parseFloat(position.z) || 0,
+        },
+        scale: {
+          x: parseFloat(scale.x) || 1,
+          y: parseFloat(scale.y) || 1,
+          z: parseFloat(scale.z) || 1,
+        },
+        rotation: {
+          x: parseFloat(rotation.x) || 0,
+          y: parseFloat(rotation.y) || 0,
+          z: parseFloat(rotation.z) || 0,
+        },
+        emissionIntensity: parseFloat(emissionValue) || 0,
       };
 
       console.log("Enviando a Unity:", JSON.stringify(modelData));
-      unityRef.current.postMessage('Object Spawner', 'LoadModelFromReact', JSON.stringify(modelData));
+      unityRef.current.postMessage('OBJImport', 'LoadModelFromReact', JSON.stringify(modelData));
+    }else {console.log("No encontrado")
     }
-  };
 
+  };
 
   const sendBloomValuesToUnity = () => {
     const bloomData = {
@@ -238,31 +215,89 @@ console.log(challengeObj)
       intensity: parseFloat(intensity),  // Captura el valor del input
     };
     if (unityRef.current) {
-      unityRef.current.postMessage('PostProcessing', 'UpdateBloomValues', JSON.stringify(bloomData));
+      unityRef.current.postMessage('PosProcessing', 'UpdateBloomValues', JSON.stringify(bloomData));
     } else {
       console.log("UnityView no está disponible");
     }
+    console.log('BloomValues:', bloomData);
   };
+
+  const captureScreenshot = () => {
+    if (unityRef.current) {
+      unityRef.current.postMessage('ScreenCapture', 'CaptureScreenshotFromReact', '');
+
+      const path = "/storage/emulated/0/Android/data/com.roam_reality/files/";
+      RNFS.readDir(path)
+        .then(files => {
+          const foundFile = files.find(file => file.isFile() && file.name.includes(".png"));
+          if (foundFile) {
+            setFileFound(foundFile.path);
+            setCaptureData(foundFile.path);
+          }
+        })
+        .catch(error => {
+          console.error("Error al leer el directorio:", error);
+        });
+    }
+  };
+
+  const shareScreenshot = async () => {
+    if (!fileFound) {
+      console.log('Primero captura una imagen antes de compartir.');
+      return;
+    }
+
+    try {
+      const shareOptions = {
+        title: 'Compartir captura',
+        url: `file://${fileFound}`,
+        type: 'image/png',
+        failOnCancel: false,
+      };
+
+      await Share.open(shareOptions);
+    } catch (error) {
+      console.error('Error al compartir:', error);
+    }
+  };
+
   return (
     <ImageBackground source={require('../../assets/images/Background.png')} style={styles.background}>
       <UnityView ref={unityRef} style={styles.unityView} />
+
       <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Threshold"
-          keyboardType="numeric"
-          value={threshold}
-          onChangeText={setThreshold}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Intensity"
-          keyboardType="numeric"
-          value={intensity}
-          onChangeText={setIntensity}
-        />
-        <Button title="Enviar" onPress={sendBloomValuesToUnity} />
-        <Button title="Enviar" onPress={sendModelDataToUnitySpawn} />
+        <Button title="Enviar Bloom" onPress={sendBloomValuesToUnity} />
+        <Button title="Capturar Pantalla" onPress={captureScreenshot} />
+        <TouchableOpacity style={styles.shareButton} onPress={shareScreenshot}>
+          <Text style={styles.shareButtonText}>Compartir Captura</Text>
+        </TouchableOpacity>
+        <View style={styles.row}>
+          <TextInput
+            style={styles.inputRow}
+            placeholder="Rotation X"
+            keyboardType="numeric"
+            value={(rotation.x)}
+            onChangeText={(value) => setRotation({ ...rotation, x: parseFloat(value) })}
+          />
+          <TextInput
+            style={styles.inputRow}
+            placeholder="Rotation Y"
+            keyboardType="numeric"
+            value={(rotation.y)}
+            onChangeText={(value) => setRotation({ ...rotation, y: parseFloat(value) })}
+          />
+          <TextInput
+            style={styles.inputRow}
+            placeholder="Rotation Z"
+            keyboardType="numeric"
+            value={(rotation.z)}
+            onChangeText={(value) => setRotation({ ...rotation, z: parseFloat(value) })}
+          />
+        </View>
+        <View style={styles.row}>
+          <Button title="Enviar Modelo a Unity" onPress={sendModelDataToUnitySpawn} />
+        </View>
+        <View style={{height:80}}></View>
       </View>
     </ImageBackground>
   );
@@ -280,7 +315,7 @@ const styles = StyleSheet.create({
   unityView: {
     borderRadius: 50,
     marginHorizontal: 20,
-    height: '60%',
+    flex:1,
     width: '100%',
   },
   inputContainer: {
@@ -290,11 +325,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  input: {
+  row: {
+    flexDirection: 'row',
+  },
+  shareButton: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  shareButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  inputRow: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    width: '100%',
+    width: 'auto',
     marginBottom: 10,
     paddingLeft: 8,
   },
