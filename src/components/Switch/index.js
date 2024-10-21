8519b90ac2503128259d43a317245d5bcdaf3aa0
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-10-21 13:20:59 -0300

    adds loading state to switch component + streamlines props usage

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -1,18 +1,12 @@
-import React, { FC, useState } from "react"
-import { Switch } from "react-native"
+import React from 'react'
+import { ActivityIndicator, Switch } from 'react-native'
 
 const AppSwitch = props => {
-  const { onValueChange, value, ...otherProps } = props
-  const [focus, setFocus] = useState(false)
-
+  if (props?.loading) {
+    return <ActivityIndicator />
+  }
   return (
-    <Switch
-      trackColor={{ false: "#9003E0", true: "#9003E0" }}
-      thumbColor={"#B816E0"}
-      // ios_backgroundColor="#9003E0"
-      onValueChange={onValueChange}
-      value={value}
-    />
+    <Switch trackColor={{ false: '#9003E0', true: '#9003E0' }} thumbColor={'#B816E0'} {...props} />
   )
 }
 
