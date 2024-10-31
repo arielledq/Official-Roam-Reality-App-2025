708b04eb70d59b6048d7e8565b8f1a734ba4e892
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-10-31 17:10:45 -0300

    updates dependencies and adds new scripts fixes

--- scripts/fix-dependencies.sh
+++ scripts/fix-dependencies.sh
@@ -1,2 +1,3 @@
 cp "./scripts/hash.hpp" "./ios/Pods/boost/boost/container_hash/hash.hpp" && wait;
+cp "./scripts/RNDateTimePickerShadowView.m" "./node_modules/@react-native-community/datetimepicker/ios/RNDateTimePickerShadowView.m" && wait;
 echo 'Updated broken dependencies files'
