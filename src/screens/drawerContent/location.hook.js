84bc3b0559f40a9632dd66f1771158e225eff8d3
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-11-28 15:13:31 -0300

    updated location handler

--- src/screens/drawerContent/location.hook.js
+++ src/screens/drawerContent/location.hook.js
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import Geolocation from 'react-native-geolocation-service'
-import { hasLocationPermission } from '../../util/LocationLib'
-import { updateUserLocationData } from '../../redux/Login'
-import { updateUserLocation } from '../../network'
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Geolocation from "react-native-geolocation-service";
+import { hasLocationPermission } from "../../util/LocationLib";
+import { updateUserLocationData } from "../../redux/Login";
+import { updateUserLocation } from "../../network";
 
 const WATCH_POSITION_CONFIG = {
   accuracy: {
-    android: 'high',
-    ios: 'best',
+    android: "high",
+    ios: "best",
   },
   enableHighAccuracy: true,
   distanceFilter: 5,
@@ -18,93 +18,99 @@ const WATCH_POSITION_CONFIG = {
   forceLocationManager: false,
   showLocationDialog: true,
   useSignificantChanges: false,
-}
+};
 
 const userLocationHook = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [initialUserLocation, setInitialUserLocation] = useState({
     latitude: null,
     longitude: null,
-  })
+  });
 
-  const userData = useSelector(state => state?.login?.data)
+  const userData = useSelector(state => state?.login?.data);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const userLocation = userData?.user?.user_ar_profile?.current_location?.coordinates
-  const locationIsEnabled = !!userLocation?.length
+  const userLocation = userData?.user?.user_ar_profile?.current_location?.coordinates;
+  const locationIsEnabled = !!userLocation?.length;
 
-  const getLocation = () => {
+  const getLocation = async () => {
+    const hasPermission = await hasLocationPermission();
+    if (!hasPermission) {
+      return;
+    }
+    setLoading(true);
+
     Geolocation.watchPosition(
       position => {
         const coords = {
           latitude: position?.coords?.latitude,
           longitude: position?.coords?.longitude,
-        }
-        setInitialUserLocation(coords)
+        };
+        setInitialUserLocation(coords);
       },
       error => {
-        console.error('[location.hook] Geolocation watchPosition error', error)
-        setLoading(false)
-        clearLocation()
+        console.error("[location.hook] Geolocation watchPosition error", error);
+        setLoading(false);
+        clearLocation();
       },
       WATCH_POSITION_CONFIG
-    )
-  }
+    );
+  };
 
   const watchLocation = async () => {
-    const hasPermission = await hasLocationPermission()
+    const hasPermission = await hasLocationPermission();
     if (!hasPermission) {
-      return
+      return;
     }
-    setLoading(true)
+    setLoading(true);
 
     Geolocation.watchPosition(
       position => {
         const coords = {
           latitude: position?.coords?.latitude,
           longitude: position?.coords?.longitude,
-        }
-        updateUserLocationAPI(coords)
-        setLoading(false)
+        };
+        updateUserLocationAPI(coords);
+        setLoading(false);
       },
       error => {
-        console.error('[location.hook] Geolocation watchPosition error', error)
-        setLoading(false)
-        clearLocation()
+        console.error("[location.hook] Geolocation watchPosition error", error);
+        setLoading(false);
+        clearLocation();
       },
       WATCH_POSITION_CONFIG
-    )
-  }
+    );
+  };
 
   const toggleUserLocation = () => {
     if (locationIsEnabled) {
-      clearLocation()
+      clearLocation();
     } else {
-      watchLocation()
+      watchLocation();
     }
-  }
+  };
 
   const updateUserLocationAPI = async ({ latitude, longitude }) => {
     if (!isNaN(latitude) && !isNaN(longitude)) {
       try {
-        await updateUserLocation({ latitude, longitude })
-        dispatch(updateUserLocationData({ latitude, longitude }))
+        await updateUserLocation({ latitude, longitude });
+        dispatch(updateUserLocationData({ latitude, longitude }));
       } catch (error) {
-        clearLocation()
-        console.error('[location.hook] updateUserLocationAPI error', error)
+        clearLocation();
+        console.error("[location.hook] updateUserLocationAPI error", error);
       }
     } else {
-      clearLocation()
-      console.error('[location.hook] location is not a number', { latitude, longitude })
+      clearLocation();
+      console.error("[location.hook] location is not a number", { latitude, longitude });
     }
-  }
+  };
 
   const clearLocation = () => {
-    updateUserLocation({ latitude: null, longitude: null })
-    dispatch(updateUserLocationData())
-    Geolocation.stopObserving()
-  }
+    updateUserLocation({ latitude: null, longitude: null });
+    dispatch(updateUserLocationData());
+    Geolocation.stopObserving();
+  };
 
   return {
     initialUserLocation,
@@ -113,7 +119,7 @@ const userLocationHook = () => {
     locationIsEnabled,
     toggleUserLocation,
     getLocation,
-  }
-}
+  };
+};
 
-export default userLocationHook
+export default userLocationHook;
