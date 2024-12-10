c265c9b5e70ba9a6c67baf5426d00a719b4ff5c8
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-12-10 15:34:34 -0300

    adjust pin challenge

--- src/screens/geoarchallenge/pinchallenge/index.js
+++ src/screens/geoarchallenge/pinchallenge/index.js
@@ -167,28 +167,28 @@ const PinChallenge = () => {
         minScale: Number(challengeObjParameters?.min_pinch_scale) || 1,
         maxScale: Number(challengeObjParameters?.max_pinch_scale) || 1,
         isRotationEnabled: true,
-        useGPS: true, // Activar GPS
-        gpsLatitude: siteLatitude || 0, // Latitud del GPS
-        gpsLongitude: siteLongitude || 0, // Longitud del GPS
+        // useGPS: true, // Activar GPS
+        // gpsLatitude: siteLatitude || 0, // Latitud del GPS
+        // gpsLongitude: siteLongitude || 0, // Longitud del GPS
       };
       console.log("AAAAAASITEEEEEEEEE", selectedGeoSite);
       console.log("Enviando datos del modelo a Unity:", modelData);
       unityRef.current.postMessage("OBJImport", "LoadModelFromReact", JSON.stringify(modelData));
-      if (modelData.useGPS) {
-        const gpsConfig = {
-          smoothingFactor: 0.1, // Factor de suavizado del GPS
-          minGPSAccuracy: 5.0, // Precisión mínima aceptable del GPS
-          scaleFactor: 1.0, // Factor de escala para las coordenadas GPS
-          maxWait: 20, // Tiempo máximo de espera para inicializar el GPS
-          isVisibleObject: true, // Controlar visibilidad inicial
-        };
-        console.log("Enviando configuración de GPS a Unity:", gpsConfig);
-        unityRef.current.postMessage(
-          "OBJImport", // GameObject que contiene el script
-          "ConfigureGPSFromReact", // Método del script
-          JSON.stringify(gpsConfig)
-        );
-      }
+      // if (modelData.useGPS) {
+      //   const gpsConfig = {
+      //     smoothingFactor: 0.1, // Factor de suavizado del GPS
+      //     minGPSAccuracy: 5.0, // Precisión mínima aceptable del GPS
+      //     scaleFactor: 1.0, // Factor de escala para las coordenadas GPS
+      //     maxWait: 20, // Tiempo máximo de espera para inicializar el GPS
+      //     isVisibleObject: true, // Controlar visibilidad inicial
+      //   };
+      //   console.log("Enviando configuración de GPS a Unity:", gpsConfig);
+      //   unityRef.current.postMessage(
+      //     "OBJImport", // GameObject que contiene el script
+      //     "ConfigureGPSFromReact", // Método del script
+      //     JSON.stringify(gpsConfig)
+      //   );
+      // }
     } else {
       console.log("UnityView o modelOBJ no están disponibles.");
     }
