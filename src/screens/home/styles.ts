6603caab138a58670a7c218dfa059a19432e2635
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-12-13 10:42:09 -0300

    add hamburger menu

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,29 +1,27 @@
-import { makeStyles } from '@rneui/themed'
-import { screenHorizontalPadding } from '../../util/AppDimensions'
-import { FontFamily, FontLineHeights, FontSizes, fontGroup } from '../../util/FontUtils'
-import { Platform } from 'react-native'
+import { makeStyles } from "@rneui/themed";
+import { FontLineHeights, FontSizes, fontGroup } from "../../util/FontUtils";
 
 const useStyles = makeStyles(theme => ({
   mainContainer: {
     flex: 1,
-    backgroundColor: '#202136',
+    backgroundColor: "#202136",
   },
   container: {
     flex: 1,
     paddingHorizontal: 25,
   },
   imageBg: {
-    width: '100%',
+    width: "100%",
     minHeight: 180,
     borderRadius: 20,
     marginBottom: 20,
     paddingVertical: 20,
-    backgroundColor: '#131422',
+    backgroundColor: "#131422",
   },
   row1: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 20,
     paddingBottom: 25,
   },
@@ -31,9 +29,9 @@ const useStyles = makeStyles(theme => ({
     flex: 1,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 20,
     paddingBottom: 25,
   },
@@ -47,7 +45,7 @@ const useStyles = makeStyles(theme => ({
     borderRadius: 20,
   },
   innerView: {
-    width: '70%',
+    width: "70%",
   },
   challengesText: {
     ...fontGroup.ns800,
@@ -71,16 +69,16 @@ const useStyles = makeStyles(theme => ({
     flex: 1,
   },
   blurView: {
-    overflow: 'hidden',
-    position: 'absolute',
+    overflow: "hidden",
+    position: "absolute",
     top: 0,
     zIndex: 10,
   },
   headerContainer: {
     paddingVertical: 15,
     borderBottomWidth: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+    backgroundColor: "rgba(0, 0, 0, 0.0)",
   },
-}))
+}));
 
-export default useStyles
+export default useStyles;
