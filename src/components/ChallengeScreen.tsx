a569fcadb8a25bc3c89b6b6315e2c3b27a333f9f
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-12-16 10:25:31 -0300

    moved background image

--- src/components/ChallengeScreen.tsx
+++ src/components/ChallengeScreen.tsx
@@ -4,7 +4,8 @@ import { screenHorizontalPadding } from "util/AppDimensions";
 import AppHeader from "./header";
 import { fontGroup, FontLineHeights, FontSizes } from "util/FontUtils";
 import theme from "assets/theme";
-import { ScrollView } from "react-native";
+import { ScrollView, StyleSheet, useWindowDimensions, View } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 interface CaptureChallengeScreenProps {
   title?: string;
@@ -19,44 +20,74 @@ const ChallengeScreen = ({
   modals,
   children,
 }: CaptureChallengeScreenProps) => {
+  const { height, width } = useWindowDimensions();
+
   return (
-    <BackgroundWithImage
-      style={{
-        flex: 1,
-      }}
-    >
-      <AppHeader
-        centerComponent={{
-          text: title,
-          numberOfLines: 2,
-          // @ts-ignore
-          style: {
-            fontSize: FontSizes.S14,
-            lineHeight: FontLineHeights.LH20,
-            color: theme.lightColors?.white,
-            textAlign: "center",
-            ...fontGroup.ns700,
-          },
-        }}
-        centerContainerStyle={{ alignItems: "center", justifyContent: "center" }}
-        backgroundColor="transparent"
-        // @ts-expect-error
-        rightComponent={headerRightComponent}
-      />
+    <>
+      <View style={{ flex: 1, backgroundColor: theme.darkColors?.inputBG }}>
+        <BackgroundWithImage
+          style={[{ width: width, height: height / 2 }, styles.backgroundStyle]}
+          imageStyle={styles.backgroundImage}
+        >
+          <LinearGradient
+            colors={["rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0.8)"]} // Transparent to semi-black
+            style={styles.gradient}
+            start={{ x: 0.5, y: 0.5 }} // Middle
+            end={{ x: 0.5, y: 1 }} // Bottom
+          />
+        </BackgroundWithImage>
 
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingBottom: 40,
-          paddingHorizontal: screenHorizontalPadding,
-        }}
-      >
-        {children}
-      </ScrollView>
+        <AppHeader
+          centerComponent={{
+            text: title,
+            numberOfLines: 2,
+            // @ts-ignore
+            style: {
+              fontSize: FontSizes.S14,
+              lineHeight: FontLineHeights.LH20,
+              color: theme.lightColors?.white,
+              textAlign: "center",
+              ...fontGroup.ns700,
+            },
+          }}
+          centerContainerStyle={{ alignItems: "center", justifyContent: "center" }}
+          backgroundColor="transparent"
+          // @ts-expect-error
+          rightComponent={headerRightComponent}
+        />
 
-      {modals}
-    </BackgroundWithImage>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 40,
+            paddingHorizontal: screenHorizontalPadding,
+          }}
+        >
+          {children}
+        </ScrollView>
+
+        {modals}
+      </View>
+    </>
   );
 };
 
 export default ChallengeScreen;
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  backgroundImage: {
+    resizeMode: "stretch",
+  },
+  gradient: {
+    ...StyleSheet.absoluteFillObject, // Covers the entire ImageBackground
+  },
+});
