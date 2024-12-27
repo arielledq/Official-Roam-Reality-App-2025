c76483b425fb96eee5a16abe0be8733e2846aeeb
restigarribia <rodney.estigarribia@innovatica.com.py>
2024-12-27 10:48:32 -0300

    add api calls to save star check in

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,6 @@ export const CHALLENGES_TYPE = {
   PHOTO_VIDEO_TITLE: "AR Challenge",
   PIN_CHECK_IN: "PIN_CHECK_IN",
   PIN_CHECK_IN_TITLE: "Location Check In Challenge",
+  STAR: "STAR",
+  STAR_TITLE: "AR Star",
 };
