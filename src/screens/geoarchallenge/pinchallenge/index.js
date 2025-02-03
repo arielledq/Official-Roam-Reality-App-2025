51a862a14a059df718a40f21cbe3ba0353a1ab4b
Victor Aguilera <victor.aguilera@innovatica.com.py>
2025-02-03 10:23:14 -0300

    full screen pinchallenge

--- src/screens/geoarchallenge/pinchallenge/index.js
+++ src/screens/geoarchallenge/pinchallenge/index.js
@@ -616,7 +616,7 @@ const PinChallenge = () => {
       appHeader = {false}
       style={{
         paddingHorizontal: 0,
-        paddingTop: 20,
+        // paddingTop: 20,
         height:'100%',
         backgroundColor: isUnityLoaded ? "#000" : theme.darkColors?.inputBG,
     }}
