a0e33b7dc74f22521035abb226013d28c59e7914
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-02-24 17:56:43 -0300

    RR-212	Waiver on sign up only - remove from before each experience.

--- src/redux/Login/reducer.js
+++ src/redux/Login/reducer.js
@@ -1,44 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   data: {},
-}
+};
 
 export const sliceLogin = createSlice({
-  name: 'login',
+  name: "login",
   initialState,
   reducers: {
     resetState: () => initialState,
     updateUserData: (state, action) => {
-      state.data = action.payload
+      state.data = action.payload;
     },
     updateUserLocationData: (state, action) => {
-      let coordinates = []
+      let coordinates = [];
       if (!isNaN(action.payload?.longitude) && !isNaN(action.payload?.latitude)) {
-        coordinates = [action.payload.longitude, action.payload.latitude]
+        coordinates = [action.payload.longitude, action.payload.latitude];
       }
       state.data.user.user_ar_profile.current_location = {
         ...state.data.user.user_ar_profile.current_location,
         coordinates: coordinates,
-      }
-    },
-    updateName: (state, action) => {
-      state.data.user.name = action.payload
+      };
     },
     updateAccountFlag: (state, action) => {
-      state.data.user.user_profile.account_setup = action.payload
+      console.log("updateAccountFlag", action);
+      return (state = {
+        ...state,
+        data: {
+          ...state?.data,
+          user: {
+            ...state?.data?.user,
+            user_profile: {
+              ...state?.data?.user?.user_profile,
+              account_setup: action.payload,
+            },
+          },
+        },
+      });
     },
     updateVerified: (state, action) => {
-      state.data.user.user_profile.is_verified = action.payload
+      state.data.user.user_profile.is_verified = action.payload;
     },
   },
-})
+});
 
 export const {
   resetState,
   updateUserData,
   updateUserLocationData,
-  updateName,
   updateAccountFlag,
   updateVerified,
-} = sliceLogin.actions
+} = sliceLogin.actions;
