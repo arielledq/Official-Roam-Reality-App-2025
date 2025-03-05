4b9666b75b81bff79cc09dda32162d4a73136174
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-03-05 10:48:26 -0300

    increase visibility time for sign up points notification

--- src/screens/geoarchallenge/index.js
+++ src/screens/geoarchallenge/index.js
@@ -9,14 +9,15 @@ import AppHeader from "../../components/header";
 import ScreenContainer from "components/ScreenContainer";
 import PanicPopUp from "./panicpopup";
 
-import {handleError, showMessage} from "../../util/helpers";
+import { handleError, showMessage } from "../../util/helpers";
 import {
   getGeoARDestinations,
   getARProfile,
   getARStettings,
   getARChallenges,
   getARSitesStars,
-  setDevice, updateProfile,
+  setDevice,
+  updateProfile,
 } from "../../network";
 import {
   updateARUserData,
@@ -33,8 +34,8 @@ import ArIcon from "../../assets/geoar/aricon.svg";
 import { MenuIcon } from "../../assets/svg";
 
 import useStyles from "./styles";
-import {GIFT_POINTS, USERS_LIMIT} from "constants";
-import {updateUserProperties} from "redux/Login/reducer";
+import { GIFT_POINTS, USERS_LIMIT } from "constants";
+import { updateUserProperties } from "redux/Login/reducer";
 
 const GeoArChallenge = ({}) => {
   const _styles = useStyles();
@@ -172,13 +173,18 @@ const GeoArChallenge = ({}) => {
   const updatePointsNotification = () => {
     updateProfile({
       id: user?.user_profile?.id,
-      data: {has_receive_points: true},
+      data: { has_receive_points: true },
     })
       .then(res => {
         if (res.status == 1) {
-          console.log("res", res)
+          console.log("res", res);
           dispatch(updateUserProperties({ has_receive_points: true }));
-          showMessage(`Surprise! We’ve added ${GIFT_POINTS} bonus points to your Roam Reality account!`);
+          showMessage(
+            `Surprise! We’ve added ${GIFT_POINTS} bonus points to your Roam Reality account!`,
+            "success",
+            null,
+            10000
+          );
         } else {
           handleError(res);
         }
@@ -200,8 +206,8 @@ const GeoArChallenge = ({}) => {
         navigation.replace("EditProfile");
       }, 300);
     }
-    if(user?.user_ar_profile?.points === GIFT_POINTS && user?.has_receive_points === false){
-      updatePointsNotification()
+    if (user?.user_ar_profile?.points === GIFT_POINTS && user?.has_receive_points === false) {
+      updatePointsNotification();
     }
   }, []);
 
