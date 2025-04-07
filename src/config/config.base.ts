7b0ec22c039e574b5b9028cf431e267fbbea2698
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-04-07 10:39:26 -0300

    update tf version

--- src/config/config.base.ts
+++ src/config/config.base.ts
@@ -25,7 +25,7 @@ const BaseConfig: ConfigBaseProps = {
   exitRoutes: ["Welcome"],
 
   APP_IOS_PROD_VERSION: "1.0.18",
-  APP_IOS_DEV_VERSION: "1.18.5",
+  APP_IOS_DEV_VERSION: "1.18.6",
 };
 
 export default BaseConfig;
