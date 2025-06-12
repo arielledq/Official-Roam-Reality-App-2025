dc12e7368ae92e6b70ef7b5f71797540e3616f71
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-06-12 16:25:36 -0300

    make the app full screen on android #206

--- app/android/app/src/main/java/com/travel_ar_app_42706/MainActivity.kt
+++ app/android/app/src/main/java/com/travel_ar_app_42706/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.roam_reality
 
 import android.os.Bundle
+import android.view.View
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -10,6 +11,23 @@ class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    hideSystemUI()
+  }
+
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    if (hasFocus) hideSystemUI()
+  }
+
+  private fun hideSystemUI() {
+    window.decorView.systemUiVisibility = (
+      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+      or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_FULLSCREEN
+    )
   }
 
   /**
