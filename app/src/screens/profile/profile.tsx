aefd337ffa8ac7bd907e813016c5ee52421c03cd
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-07-17 13:19:09 -0300

    removed navigation from profile into scoreboard

--- app/src/screens/profile/profile.tsx
+++ app/src/screens/profile/profile.tsx
@@ -311,14 +311,7 @@ const Profile: ScreenStackComponent<RootStackParamList, "Profile"> = () => {
           isVerified={userProfile?.user_profile?.is_verified}
         />
         <View style={_styles.scoreboardContainer}>
-          <AppText
-            adjustsFontSizeToFit={true}
-            numberOfLines={1}
-            // @ts-ignore
-            onPress={() => navigation.navigate("ScoreBoard")}
-            // @ts-ignore
-            style={_styles.scoreboard}
-          >
+          <AppText adjustsFontSizeToFit={true} numberOfLines={1} style={_styles.scoreboard}>
             SCOREBOARD
           </AppText>
         </View>
