24c46fac5d4983b628db6a696432a6847c35df42
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-07-30 15:47:51 -0300

    update ios + android vers

--- app/src/config/config.base.ts
+++ app/src/config/config.base.ts
@@ -25,7 +25,7 @@ const BaseConfig: ConfigBaseProps = {
   exitRoutes: ["Welcome"],
 
   APP_IOS_PROD_VERSION: "1.0.25",
-  APP_IOS_DEV_VERSION: "1.25.7",
+  APP_IOS_DEV_VERSION: "1.25.8",
 };
 
 export default BaseConfig;
