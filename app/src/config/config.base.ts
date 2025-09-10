6761a28b91dab2313ae2906fb109333af8c29759
victor <victor.aguilera@innovatica.com.py>
2025-09-10 13:11:46 -0300

    Upgrade version ios

--- app/src/config/config.base.ts
+++ app/src/config/config.base.ts
@@ -25,7 +25,7 @@ const BaseConfig: ConfigBaseProps = {
   exitRoutes: ["Welcome"],
 
   APP_IOS_PROD_VERSION: "1.0.25",
-  APP_IOS_DEV_VERSION: "1.25.17",
+  APP_IOS_DEV_VERSION: "1.25.18",
 };
 
 export default BaseConfig;
