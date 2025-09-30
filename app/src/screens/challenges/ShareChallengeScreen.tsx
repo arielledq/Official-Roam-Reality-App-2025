e890843cc9ddd503205c5b76acddf6393984f8bf
restigarribia <rodney.estigarribia@innovatica.com.py>
2025-09-30 10:08:56 -0300

    cleaned up styles + imports

--- app/src/screens/challenges/ShareChallengeScreen.tsx
+++ app/src/screens/challenges/ShareChallengeScreen.tsx
@@ -1,9 +1,7 @@
 import React, {useContext, useEffect, useRef, useState} from "react";
-import {Image, Platform, Text, View, Dimensions} from "react-native";
+import {Image, Platform, Text, View, Dimensions, StyleSheet} from "react-native";
 import {useNavigation, useRoute} from "@react-navigation/native";
 import moment from "moment";
-// @ts-ignore
-import Video from "react-native-video";
 import {useDispatch} from "react-redux";
 import {RouteProp} from "@react-navigation/native";
 
@@ -12,12 +10,12 @@ import {
   getARProfile,
   postArMemory,
   postGeoPinCheckIn,
-  starFoundAndSaveApi,
+  // starFoundAndSaveApi,
   getNextStar as getNextStarApi,
   updateUserPointAPI,
 } from "network";
 import {fontGroup, FontSizes} from "util/FontUtils";
-import {getFileExtension, handleError, saveToGallery, showMessage} from "util/helpers";
+import {getFileExtension, handleError, saveToGallery} from "util/helpers";
 // @ts-ignore
 import {CHALLENGES_TYPE} from "constants";
 import {updateARUserData} from "../../redux/AR";
@@ -86,9 +84,6 @@ const ArChallengeShare = () => {
   const startDate = isMemory
     ? moment(challengeObj?.created_at).format("MM-DD-YYYY")
     : moment().format("MM-DD-YYYY");
-  console.log("scan_picture", scan_picture);
-  console.log("startDate", startDate);
-  console.log("challengeObj?.created_at", challengeObj?.created_at);
   let endChallengeButtonText = "End & Share to Roam Profile";
 
   switch (challengeType) {
@@ -218,53 +213,50 @@ const ArChallengeShare = () => {
     let res;
 
     try {
-      if (challengeType === CHALLENGES_TYPE.PHOTO_VIDEO) {
-        formData.append("challenges", challengeObj?.id);
-        formData.append("memory_file", shareFile);
-        let memoryType = "";
-        if (challengeObj?.memory_type) {
-          memoryType = challengeObj?.memory_type;
-        } else {
-          memoryType = fileExt == "mp4" ? "VIDEO" : "PHOTO";
-        }
-        formData.append("memory_type", memoryType);
-        res = await postArMemory(formData);
-      }
-      if (challengeType === CHALLENGES_TYPE.PIN_CHECK_IN) {
-        formData.append("geo_challenge", challengeObj?.id);
-        formData.append("geo_site", challengeObj?.geo_site?.id);
-        formData.append("memory_file", shareFile);
+      switch (challengeType) {
+        case CHALLENGES_TYPE.PHOTO_VIDEO:
+          formData.append("challenges", challengeObj?.id);
+          formData.append("memory_file", shareFile);
+          let memoryType = "";
+          if (challengeObj?.memory_type) {
+            memoryType = challengeObj?.memory_type;
+          } else {
+            memoryType = fileExt == "mp4" ? "VIDEO" : "PHOTO";
+          }
+          formData.append("memory_type", memoryType);
+          res = await postArMemory(formData);
+          break;
 
-        res = await postGeoPinCheckIn(formData);
-      }
-      // if (challengeType === AR_MODES.SCAN_MODE) {
-      //   formData.append("scan_id", challengeObj?.scanChallenge?.id);
-      //   shareFile = {
-      //     ...shareFile,
-      //     uri: capturedDataUri,
-      //   };
-      //   formData.append("memory_file", shareFile);
-      //   formData.append("memory_type", challengeObj?.memory_type);
-      //   res = await postArMemory(formData);
-      // }
+        case CHALLENGES_TYPE.PIN_CHECK_IN:
+          formData.append("geo_challenge", challengeObj?.id);
+          formData.append("geo_site", challengeObj?.geo_site?.id);
+          formData.append("memory_file", shareFile);
 
-      if (challengeType === AR_MODES.SCAN_MODE) {
-        formData.append("scan_id", challengeObj?.scanChallenge?.id);
-        const fixedShareFile = {...shareFile, uri: capturedDataUri};
-        formData.append("memory_file", fixedShareFile);
-        formData.append("memory_type", "SCAN_PHOTO");
-        res = await postArMemory(formData);
-      }
-      if (challengeType === AR_MODES.GEO_TAG_MODE) {
-        formData.append("geo_challenge", challengeObj?.pin_challenge?.id);
-        formData.append("geo_site", challengeObj?.id);
-        shareFile = {
-          ...shareFile,
-          uri: capturedDataUri,
-        };
-        formData.append("memory_file", shareFile);
+          res = await postGeoPinCheckIn(formData);
+          break;
 
-        res = await postGeoPinCheckIn(formData);
+        case AR_MODES.SCAN_MODE:
+          formData.append("scan_id", challengeObj?.scanChallenge?.id);
+          const fixedShareFile = {...shareFile, uri: capturedDataUri};
+          formData.append("memory_file", fixedShareFile);
+          formData.append("memory_type", "SCAN_PHOTO");
+          res = await postArMemory(formData);
+          break;
+
+        case AR_MODES.GEO_TAG_MODE:
+          formData.append("geo_challenge", challengeObj?.pin_challenge?.id);
+          formData.append("geo_site", challengeObj?.id);
+          shareFile = {
+            ...shareFile,
+            uri: capturedDataUri,
+          };
+          formData.append("memory_file", shareFile);
+
+          res = await postGeoPinCheckIn(formData);
+          break;
+
+        default:
+          break;
       }
 
       if (res?.status === 1) {
@@ -284,17 +276,6 @@ const ArChallengeShare = () => {
       setIsLoading(false);
     }
   };
-
-  useEffect(() => {
-    if (
-      !hasSharedToRoamProfile &&
-      (socialPointsCounter.facebook === 1 ||
-        socialPointsCounter.instagram === 1 ||
-        socialPointsCounter.others === 1)
-    ) {
-      shareToRoamProfile();
-    }
-  }, [socialPointsCounter, hasSharedToRoamProfile]);
 
   const ARUserProfile = () => {
     getARProfile()
@@ -391,6 +372,25 @@ const ArChallengeShare = () => {
     setViewHeight(height);
   };
 
+  const toggleLoading = (value: boolean) => {
+    if (isMemory) {
+      setIsLoadingDisplay(value);
+    } else {
+      setIsLoadingDisplay(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      !hasSharedToRoamProfile &&
+      (socialPointsCounter.facebook === 1 ||
+        socialPointsCounter.instagram === 1 ||
+        socialPointsCounter.others === 1)
+    ) {
+      shareToRoamProfile();
+    }
+  }, [socialPointsCounter, hasSharedToRoamProfile]);
+
   useEffect(() => {
     if (
       challengeObj?.selectedMode?.mode === AR_MODES.SCAN_MODE ||
@@ -401,10 +401,11 @@ const ArChallengeShare = () => {
     }
   }, [challengeObj]);
 
-  let shareButtonTextSize = FontSizes.S16;
-  if (width < 420) {
-    shareButtonTextSize = FontSizes.S12;
-  }
+  useEffect(() => {
+    if (!isMemory) {
+      updateUserPointAPI({points: challengePoints});
+    }
+  }, [isMemory]);
 
   const screenModals = (
     <>
@@ -421,232 +422,229 @@ const ArChallengeShare = () => {
     </>
   );
 
-  const toggleLoading = (value: boolean) => {
-    if (isMemory) {
-      setIsLoadingDisplay(value);
-    } else {
-      setIsLoadingDisplay(false);
-    }
-  };
-
-  useEffect(() => {
-    if (!isMemory) {
-      updateUserPointAPI({points: challengePoints});
-    }
-  }, [isMemory]);
+  const shareButtonTextSize = width < 420 ? FontSizes.S12 : FontSizes.S16;
 
   return (
     <ChallengeScreen
       title={screenTitle}
-      style={{justifyContent: "space-between", flex: 1}}
+      style={styles.screenContainer}
       modals={screenModals}
       disableBackButton={disableBackButton}
       hideBackButton={hideBackButton}
     >
-      <View style={{flex: 1, paddingHorizontal: 32}}>
-        <View style={{flex: 1}}>
-          {challengeTitle && (
-            <View style={{flexDirection: "row", gap: 12}}>
-              {/* Points box */}
-              <View
-                style={{
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  borderRadius: 8,
-                  backgroundColor: "transparent",
-                  width: 55,
-                  height: 55,
-                }}
-              >
-                <BackgroundWithImage
-                  imageSource={BGArShare}
-                  style={{
-                    backgroundColor: "transparent",
-                    position: "absolute",
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                  }}
-                ></BackgroundWithImage>
-                <AppText
-                  style={{
-                    ...fontGroup.nunitoBold,
-                    fontWeight: "900",
-                    fontSize: FontSizes.S24,
-                    color: theme.lightColors?.white,
-                    margin: 0,
-                  }}
-                >
-                  {challengePoints || 0}
-                </AppText>
-                <AppText
-                  style={{
-                    ...fontGroup.nunitoRegular,
-                    fontWeight: "400",
-                    fontSize: FontSizes.S10,
-                    color: theme.lightColors?.white,
-                  }}
-                >
-                  Points
-                </AppText>
-              </View>
-              <AppText
-                numberOfLines={3}
-                style={{
-                  ...fontGroup.nunitoBold,
-                  fontWeight: "900",
-                  fontSize: FontSizes.S18,
-                  color: theme.lightColors?.white,
-                  flex: 1,
-                }}
-              >
-                {challengeTitle}
-              </AppText>
+      <View style={styles.content}>
+        {challengeTitle && (
+          <View style={styles.titleContainer}>
+            <View style={styles.pointsBox}>
+              <BackgroundWithImage
+                imageSource={BGArShare}
+                style={styles.pointsBoxBackground}
+              ></BackgroundWithImage>
+              <AppText style={styles.pointsBoxText}>{challengePoints || 0}</AppText>
+              <AppText style={styles.pointsBoxTextTitle}>Points</AppText>
             </View>
-          )}
+            <AppText numberOfLines={3} style={styles.titleText}>
+              {challengeTitle}
+            </AppText>
+          </View>
+        )}
 
+        <View style={styles.mediaContainer}>
           <View
-            style={{
-              flex: 1,
-              backgroundColor: "#272741",
-              gap: 8,
-              paddingVertical: 8,
-              marginTop: 16,
-              marginBottom: 8,
-              borderRadius: 12,
-              alignItems: "center",
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                width: "100%",
-                height: "100%",
-                justifyContent: "center",
+            style={[
+              {
                 opacity: isLoadingDisplay ? 0 : 1,
-              }}
-              ref={viewRef}
-              onLayout={handleLayout}
-            >
-              <ResponsiveMedia
-                source={{uri: capturedDataUri}}
-                containerHeight={viewHeight}
-                isImage={fileExt !== "mp4" && !isVideo}
-                onLoadEnd={() => toggleLoading(false)}
-              />
-            </View>
-
-            <View
-              style={{
-                width: "100%",
-                height: 55,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              {/* Sponsor row */}
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Image style={{width: 20, height: 20, marginEnd: 8}} source={{uri: sponsorImage}} />
-                <Text
-                  style={{
-                    ...fontGroup.nunitoBold,
-                    fontWeight: "700",
-                    fontSize: FontSizes.S20,
-                    color: theme.lightColors?.white,
-                  }}
-                >
-                  {sponsorName}
-                </Text>
-              </View>
-
-              {/* Completion date */}
-              {/* {!scan_picture && ( */}
-              <Text
-                style={{
-                  ...fontGroup.nunitoLight,
-                  fontWeight: "300",
-                  fontSize: FontSizes.S10,
-                  color: theme.lightColors?.white,
-                }}
-              >
-                Completed on: {startDate}
-              </Text>
-              {/* )} */}
-            </View>
-
-            <FullScreenLoadingSpinner isLoading={isLoadingDisplay} />
-          </View>
-
-          <View
-            style={{
-              flexDirection: "column",
-              gap: 16,
-              alignItems: "center",
-              marginTop: 8,
-              marginBottom: 16,
-            }}
+              },
+              styles.mediaResponsiveContainer,
+            ]}
+            ref={viewRef}
+            onLayout={handleLayout}
           >
-            {!isMemory && (
-              <Text
-                style={{
-                  fontSize: FontSizes.S12,
-                  color: theme.lightColors?.grey0,
-                }}
-              >
-                {SHARE_CONDITIONS_TEXT}
-              </Text>
-            )}
+            <ResponsiveMedia
+              source={{uri: capturedDataUri}}
+              containerHeight={viewHeight}
+              isImage={fileExt !== "mp4" && !isVideo}
+              onLoadEnd={() => toggleLoading(false)}
+            />
           </View>
+
+          <View style={styles.mediaFooterContainer}>
+            <View style={styles.mediaFooterSponsorContainer}>
+              <Image style={styles.mediaFooterSponsorImage} source={{uri: sponsorImage}} />
+              <Text style={styles.mediaFooterSponsorText}>{sponsorName}</Text>
+            </View>
+
+            <Text style={styles.mediaFooterCompletionDateText}>Completed on: {startDate}</Text>
+          </View>
+
+          <FullScreenLoadingSpinner isLoading={isLoadingDisplay} />
         </View>
-        <View style={{gap: 8, height: 110}}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              gap: 16,
+
+        <View style={styles.conditionsContainer}>
+          {!isMemory && <Text style={styles.conditionsText}>{SHARE_CONDITIONS_TEXT}</Text>}
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <View
+          style={[
+            {
               marginTop: isMemory ? 16 : 0,
-              height: 55,
-            }}
-          >
-            {/* Share to socials button */}
-            <AppButton
-              onPress={shareToSocialMediaButtonHandler}
-              containerStyle={{flex: 1, justifyContent: "center"}}
-              titleStyle={{fontSize: shareButtonTextSize, fontWeight: "bold"}}
-              title={"Share To Socials"}
-            />
+            },
+            styles.footerButtonContainer,
+          ]}
+        >
+          {/* Share to socials button */}
+          <AppButton
+            onPress={shareToSocialMediaButtonHandler}
+            containerStyle={styles.footerButton}
+            titleStyle={{fontSize: shareButtonTextSize, fontWeight: "bold"}}
+            title={"Share To Socials"}
+          />
 
-            <AppButton
-              onPress={saveToGalleryButtonHandler}
-              containerStyle={{flex: 1, justifyContent: "center"}}
-              titleStyle={{fontSize: shareButtonTextSize, fontWeight: "bold"}}
-              title={isVideo ? "Save Video" : "Save Image"}
-            />
-          </View>
-          {!isMemory && (
-            <AppButton
-              onPress={endShareProfileButtonHandler}
-              buttonStyle={{height: 55}}
-              containerStyle={{flex: 1}}
-              titleStyle={{fontSize: FontSizes.S18, fontWeight: "bold"}}
-              title={endChallengeButtonText}
-              loading={isLoading}
-              disabled={isLoading}
-            />
-          )}
+          <AppButton
+            onPress={saveToGalleryButtonHandler}
+            containerStyle={styles.footerButton}
+            titleStyle={{fontSize: shareButtonTextSize, fontWeight: "bold"}}
+            title={isVideo ? "Save Video" : "Save Image"}
+          />
         </View>
+        {!isMemory && (
+          <AppButton
+            onPress={endShareProfileButtonHandler}
+            buttonStyle={{height: 55}}
+            containerStyle={{flex: 1}}
+            titleStyle={{fontSize: FontSizes.S18, fontWeight: "bold"}}
+            title={endChallengeButtonText}
+            loading={isLoading}
+            disabled={isLoading}
+          />
+        )}
       </View>
     </ChallengeScreen>
   );
 };
 
 export default ArChallengeShare;
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    justifyContent: "space-between",
+    flex: 1,
+    paddingHorizontal: 32,
+  },
+  content: {
+    flex: 1,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 12,
+  },
+  pointsBox: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    backgroundColor: "transparent",
+    width: 55,
+    height: 55,
+  },
+  pointsBoxBackground: {
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  pointsBoxText: {
+    ...fontGroup.nunitoBold,
+    fontWeight: "900",
+    fontSize: FontSizes.S24,
+    color: theme.lightColors?.white,
+    margin: 0,
+  },
+  pointsBoxTextTitle: {
+    ...fontGroup.nunitoRegular,
+    fontWeight: "400",
+    fontSize: FontSizes.S10,
+    color: theme.lightColors?.white,
+  },
+  titleText: {
+    ...fontGroup.nunitoBold,
+    fontWeight: "900",
+    fontSize: FontSizes.S18,
+    color: theme.lightColors?.white,
+    flex: 1,
+  },
+  mediaContainer: {
+    flex: 1,
+    backgroundColor: "#272741",
+    gap: 8,
+    paddingVertical: 8,
+    marginTop: 16,
+    marginBottom: 8,
+    borderRadius: 12,
+    alignItems: "center",
+  },
+  mediaResponsiveContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+  },
+  mediaFooterContainer: {
+    width: "100%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mediaFooterSponsorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mediaFooterSponsorImage: {
+    width: 20,
+    height: 20,
+    marginEnd: 8,
+  },
+  mediaFooterSponsorText: {
+    ...fontGroup.nunitoBold,
+    fontWeight: "700",
+    fontSize: FontSizes.S20,
+    color: theme.lightColors?.white,
+  },
+  mediaFooterCompletionDateText: {
+    ...fontGroup.nunitoLight,
+    fontWeight: "300",
+    fontSize: FontSizes.S10,
+    color: theme.lightColors?.white,
+  },
+  conditionsContainer: {
+    flexDirection: "column",
+    gap: 16,
+    alignItems: "center",
+    marginTop: 8,
+    marginBottom: 16,
+  },
+  conditionsText: {
+    fontSize: FontSizes.S12,
+    color: theme.lightColors?.grey0,
+  },
+  footer: {
+    gap: 8,
+    height: 110,
+  },
+  footerButtonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 16,
+    height: 55,
+  },
+  footerButton: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
