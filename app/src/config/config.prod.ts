6cbc56f88521884c3b849e7ef3d55a7e3eb26816
Your Name <abdul.basit@ropstam.com>
2025-10-16 19:25:27 +0500

    change url

--- app/src/config/config.prod.ts
+++ app/src/config/config.prod.ts
@@ -10,8 +10,8 @@ export default {
   // BASE_URL: "https://travel-ar-app-42706-32103.azurewebsites.net/", // Production Azure
   // APPLE_REDIRECT_URL:
   //   "https://travel-ar-app-42706-32103.azurewebsites.net/accounts/apple/login/callback/", // Production Azure
-  // BASE_URL: "https://travel-ar-app-42706-staging.azurewebsites.net/", // Staging Azure
-  BASE_URL: "https://faa1f97aa27d.ngrok-free.app", // Production Azure
+  BASE_URL: "https://travel-ar-app-42706-staging.azurewebsites.net/", // Staging Azure
+  // BASE_URL: "https://faa1f97aa27d.ngrok-free.app", // Production Azure
   APPLE_REDIRECT_URL:
     "https://travel-ar-app-42706-staging.azurewebsites.net/accounts/apple/login/callback/", // Staging Azure
   ONE_SIGNAL_APP_ID: "ceda5dd2-8fd2-4180-9532-7b7f127a612d",
