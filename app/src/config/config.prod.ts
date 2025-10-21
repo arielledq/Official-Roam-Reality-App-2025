023ecbc49bf81c2ef53cf68f1eee3c8f92a6f67e
Your Name <abdul.basit@ropstam.com>
2025-10-21 15:52:18 +0500

    change url

--- app/src/config/config.prod.ts
+++ app/src/config/config.prod.ts
@@ -9,9 +9,10 @@ export default {
   APPLE_CLIENT_ID: "com.roam.reality.services",
   // BASE_URL: "https://travel-ar-app-42706-32103.azurewebsites.net/", // Production Azure
   // APPLE_REDIRECT_URL:
-  //   "https://travel-ar-app-42706-32103.azurewebsites.net/accounts/apple/login/callback/", // Production Azure
-  BASE_URL: "https://travel-ar-app-42706-staging.azurewebsites.net/", // Staging Azure
+  // //   "https://travel-ar-app-42706-32103.azurewebsites.net/accounts/apple/login/callback/", // Production Azure
+  // BASE_URL: "https://travel-ar-app-42706-staging.azurewebsites.net/", // Staging Azure
   // BASE_URL: "https://faa1f97aa27d.ngrok-free.app", // Production Azure
+  BASE_URL: "https://roam-reality-dev-api.ropstam.dev/",
   APPLE_REDIRECT_URL:
     "https://travel-ar-app-42706-staging.azurewebsites.net/accounts/apple/login/callback/", // Staging Azure
   ONE_SIGNAL_APP_ID: "ceda5dd2-8fd2-4180-9532-7b7f127a612d",
