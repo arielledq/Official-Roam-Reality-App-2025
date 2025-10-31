cc9ffd1c1fda65994aa25569cff066811995eee0
masadaq <masadaq.masood@ropstam.com>
2025-10-31 15:09:57 +0500

    Enable edit mode

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -215,8 +215,8 @@ class ModeAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
     
-    def has_change_permission(self, request, obj=None):
-        return False
+    # def has_change_permission(self, request, obj=None):
+    #     return False
     
     def has_delete_permission(self, request, obj=None):
         return False
